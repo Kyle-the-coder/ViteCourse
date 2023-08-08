f1128f13934e81e32a61a736385a492e0af80ef8
Kyle Mitchell <kylethacoder@gmail.com>
2023-08-08 10:44:06 -0700

    updated

--- viteState/src/Array.jsx
+++ viteState/src/Array.jsx
@@ -26,26 +26,57 @@ const ArrayChallenge = () => {
     setLetterArr([]);
   };
 
+  const handleResetLetterArr = () => {
+    setLetterArr(["A", "B", "C"]);
+  };
+
+  const handleAToH = (letter) => {
+    setLetterArr((currentArray) => {
+      return currentArray.map((element) => {
+        if (element === letter) return "H";
+        return element;
+      });
+    });
+  };
+
   return (
     <div>
       <div>
-        <button onClick={handleRemoveFirstItem}>Remove First Item</button>
-        <input
-          type="text"
-          onChange={(e) => setSpecificLetter(e.target.value)}
-        />
-        <button onClick={() => handleRemoveSpecificLetter(specificLetter)}>
-          Click to remove your letter
-        </button>
-        <input type="text" onChange={(e) => setNewLetter(e.target.value)} />
-        <button onClick={() => handleAddNewLetterBeg(newLetter)}>
-          Click to add your letter to the beginning
-        </button>
-        <input type="text" onChange={(e) => setNewLetter(e.target.value)} />
-        <button onClick={() => handleAddNewLetterEnd(newLetter)}>
-          Click to add your letter to the End
-        </button>
-        <button onClick={handleClearLetterArr}>Click to clear</button>
+        <div>
+          <button onClick={handleRemoveFirstItem}>Remove First Item</button>
+        </div>
+        <div>
+          <input
+            type="text"
+            onChange={(e) => setSpecificLetter(e.target.value)}
+          />
+          <button onClick={() => handleRemoveSpecificLetter(specificLetter)}>
+            Click to remove your letter
+          </button>
+        </div>
+        <div>
+          <input type="text" onChange={(e) => setNewLetter(e.target.value)} />
+          <button onClick={() => handleAddNewLetterBeg(newLetter)}>
+            Click to add your letter to the beginning
+          </button>
+        </div>
+        <div>
+          <input type="text" onChange={(e) => setNewLetter(e.target.value)} />
+          <button onClick={() => handleAddNewLetterEnd(newLetter)}>
+            Click to add your letter to the End
+          </button>
+        </div>
+        <div>
+          <button onClick={handleClearLetterArr}>Click to clear</button>
+        </div>
+        <div>
+          <button onClick={handleResetLetterArr}>Click to reset</button>
+        </div>
+        <div>
+          <button onClick={() => handleAToH("A")}>
+            Click to change A to H
+          </button>
+        </div>
       </div>
       {letterArr.join(", ")}
     </div>

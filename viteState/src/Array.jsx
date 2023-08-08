2e75deaf28d2279d382b5afbfe4c147db95b5d98
Kyle Mitchell <kylethacoder@gmail.com>
2023-08-08 09:54:43 -0700

    updated

--- viteState/src/Array.jsx
+++ viteState/src/Array.jsx
@@ -1,5 +1,19 @@
+import { useState } from "react";
 const ArrayChallenge = () => {
-  return <div></div>;
+  const [letterArr, setLetterArr] = useState(["A", "B", "C"]);
+
+  const handleRemoveFirstItem = () => {
+    setLetterArr((currentArray) => currentArray.slice(1));
+  };
+
+  return (
+    <div>
+      <div>
+        <button onClick={handleRemoveFirstItem}>Remove First Item</button>
+      </div>
+      {letterArr.join(", ")}
+    </div>
+  );
 };
 
 export default ArrayChallenge;

4baef33f72bf2031d2728c98dabea688ced4e972
Kyle Mitchell <kylethacoder@gmail.com>
2023-08-08 10:53:03 -0700

    updated

--- viteState/src/App.jsx
+++ viteState/src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from "react";
 import ArrayChallenge from "./Array";
 import Counter from "./Counter";
+import NameAge from "./NameAge";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <ArrayChallenge />
+      <NameAge />
     </>
   );
 }

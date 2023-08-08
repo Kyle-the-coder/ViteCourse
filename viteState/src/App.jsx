bedb201f70790e331aabab795f15aebed4b7225e
Kyle Mitchell <kylethacoder@gmail.com>
2023-08-08 11:44:42 -0700

    updated

--- viteState/src/App.jsx
+++ viteState/src/App.jsx
@@ -5,10 +5,13 @@ import NameAge from "./NameAge";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [isShown, setIsShown] = useState(false);
+  const childShown = isShown ? <NameAge /> : null;
 
   return (
     <>
-      <NameAge />
+      <button onClick={() => setIsShown((s) => !s)}>Show/Hide</button>
+      {childShown}
     </>
   );
 }

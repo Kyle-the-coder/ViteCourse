40529998262770e493121fc5e2b8d246239f2087
Kyle Mitchell <kylethacoder@gmail.com>
2023-08-11 10:27:21 -0700

    updated

--- viteCustomFetchHook/src/useFetch.js
+++ viteCustomFetchHook/src/useFetch.js
@@ -1,18 +1,39 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
-export function useFetch(initialValue) {
-  const [urlResults, setUrlResults] = useState(initialValue);
+export function useFetch(url) {
+  const [data, setData] = useState();
+  const [isErr, setIsErr] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
-  fetch(initialValue)
-    .then((res) => {
-      if (res.status === 200) {
-        res.json();
-      }
-    })
-    .then((data) => {
-      console.log(data);
-    })
-    .catch((err) => console.log(err));
+  useEffect(() => {
+    setData(undefined);
+    setIsErr(false);
+    setIsLoading(true);
 
-  return urlResults;
+    const controller = new AbortController();
+    fetch(url, { signal: controller.signal })
+      .then((res) => {
+        if (res.status === 200) {
+          return res.json();
+        } else {
+          return Promise.reject(res);
+        }
+      })
+      .then(setData)
+      .catch((err) => {
+        if (err.name === "Abort Error") {
+          return;
+        }
+        setIsErr(true);
+      })
+      .finally(() => {
+        if (controller.signal.aborted) return;
+        setIsLoading(false);
+      });
+    return () => {
+      controller.abort();
+    };
+  }, [url]);
+
+  return { data, isLoading, isErr };
 }

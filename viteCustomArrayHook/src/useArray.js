0487fe0aed07bb19875be4ed5853c74e3d0604e0
Kyle Mitchell <kylethacoder@gmail.com>
2023-08-11 11:38:26 -0700

    updated

--- viteCustomArrayHook/src/useArray.js
+++ viteCustomArrayHook/src/useArray.js
@@ -1,11 +1,42 @@
-import { useState } from "react";
-export function useArray(
-  array,
-  set,
-  push,
-  replace,
-  filter,
-  remove,
-  clear,
-  reset
-) {}
+import { useState, useEffect } from "react";
+
+export function useArray(INITIAL_ARRAY) {
+  const [array, setArray] = useState(INITIAL_ARRAY);
+
+  const push = (item) => {
+    setArray((prevArr) => [...prevArr, item]);
+  };
+
+  const replace = (index, newItem) => {
+    setArray((prevArr) => {
+      return [...prevArr.slice(0, index), newItem, ...prevArr.slice(index + 1)];
+    });
+  };
+
+  const filter = (item) => {
+    setArray((prevArr) => {
+      return prevArr.filter(item);
+    });
+  };
+
+  const remove = (index) => {
+    setArray((prevArr) => {
+      return [...prevArr.slice(0, index), ...prevArr.slice(index + 1)];
+    });
+  };
+
+  const clear = () => setArray([]);
+
+  const reset = () => setArray(INITIAL_ARRAY);
+
+  return {
+    array,
+    set: setArray,
+    push,
+    replace,
+    filter,
+    remove,
+    clear,
+    reset,
+  };
+}

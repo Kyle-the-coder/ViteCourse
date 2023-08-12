7bd0dd46c9aa218e3ca0d5482b9dad91b1b1b3aa
Kyle Mitchell <kylethacoder@gmail.com>
2023-08-12 09:57:53 -0700

    updated

--- viteCustomArrayHook/src/useArray.js
+++ viteCustomArrayHook/src/useArray.js
@@ -1,33 +1,33 @@
-import { useState, useEffect } from "react";
+import { useState, useCallback } from "react";
 
 export function useArray(INITIAL_ARRAY) {
   const [array, setArray] = useState(INITIAL_ARRAY);
 
-  const push = (item) => {
+  const push = useCallback((item) => {
     setArray((prevArr) => [...prevArr, item]);
-  };
+  }, []);
 
-  const replace = (index, newItem) => {
+  const replace = useCallback((index, newItem) => {
     setArray((prevArr) => {
       return [...prevArr.slice(0, index), newItem, ...prevArr.slice(index + 1)];
     });
-  };
+  }, []);
 
-  const filter = (item) => {
+  const filter = useCallback((item) => {
     setArray((prevArr) => {
       return prevArr.filter(item);
     });
-  };
+  }, []);
 
-  const remove = (index) => {
+  const remove = useCallback((index) => {
     setArray((prevArr) => {
       return [...prevArr.slice(0, index), ...prevArr.slice(index + 1)];
     });
-  };
+  }, []);
 
-  const clear = () => setArray([]);
+  const clear = useCallback(() => setArray([]), []);
 
-  const reset = () => setArray(INITIAL_ARRAY);
+  const reset = useCallback(() => setArray(INITIAL_ARRAY), [INITIAL_ARRAY]);
 
   return {
     array,

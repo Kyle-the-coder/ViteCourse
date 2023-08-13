6ecc566f034a2e38b07b649d3a3e4510b4127f06
Kyle Mitchell <kylethacoder@gmail.com>
2023-08-13 10:26:41 -0700

    updated

--- viteFormValidation/src/App.jsx
+++ viteFormValidation/src/App.jsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <form class="form">
+          <div class="form-group error">
+            <label class="label" for="email">
+              Email
+            </label>
+            <input
+              class="input"
+              type="email"
+              id="email"
+              value="test@test.com"
+            />
+            <div class="msg">Must end in @webdevsimplified.com</div>
+          </div>
+          <div class="form-group">
+            <label class="label" for="password">
+              Password
+            </label>
+            <input
+              class="input"
+              value="Password123!"
+              type="password"
+              id="password"
+            />
+          </div>
+          <button class="btn" type="submit">
+            Submit
+          </button>
+        </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

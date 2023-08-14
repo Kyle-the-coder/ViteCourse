4e9b29be4cd4a8f7f8809e3915e2cec521285b3f
Kyle Mitchell <kylethacoder@gmail.com>
2023-08-14 09:13:39 -0700

    updated

--- viteFormValidationUpgrade/src/Forms/StateForm.jsx
+++ viteFormValidationUpgrade/src/Forms/StateForm.jsx
@@ -1,8 +1,40 @@
 import { useState } from "react";
+import "../styles/styles.css";
 const StateForm = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <div>
-      <h1>hello from state form</h1>
+      <form className="form">
+        <div className="form-group error">
+          <label className="label" for="email">
+            Email
+          </label>
+          <input
+            className="input"
+            type="email"
+            id="email"
+            value="test@test.com"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <div className="msg">Must end in @webdevsimplified.com</div>
+        </div>
+        <div className="form-group">
+          <label className="label" for="password">
+            Password
+          </label>
+          <input
+            className="input"
+            value="Password123!"
+            type="password"
+            id="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button className="btn" type="submit">
+          Submit
+        </button>
+      </form>
     </div>
   );
 };

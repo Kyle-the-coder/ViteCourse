f925dc3386a252f150134fc7973c9d153b731231
Kyle Mitchell <kylethacoder@gmail.com>
2023-08-14 09:48:57 -0700

    updated

--- viteFormValidationUpgrade/src/validators.js
+++ viteFormValidationUpgrade/src/validators.js
@@ -1,4 +1,4 @@
-function checkEmail(email) {
+export function checkEmail(email) {
   const errors = [];
   if (email.length === 0) {
     errors.push("Required");
@@ -6,12 +6,23 @@ function checkEmail(email) {
   if (!email.endsWith("@webdevsimplified.com")) {
     errors.push("Must end with @webdevsimplified.com");
   }
+  return errors;
 }
 
-function checkPassword(password) {
+export function checkPassword(password) {
   const errors = [];
 
   if (password.length < 10) {
     errors.push("password must be at least 10 characters");
   }
+  if (!password.match(/[a-z]/)) {
+    errors.push("password must include a lowercase letter");
+  }
+  if (!password.match(/[A-Z]/)) {
+    errors.push("password must include a uppercase letter");
+  }
+  if (!password.match(/[0-9]/)) {
+    errors.push("password must include a number");
+  }
+  return errors;
 }

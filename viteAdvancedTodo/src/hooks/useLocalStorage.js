f78bc6742142640c0475c59eeb24760803c77749
Kyle Mitchell <kylethacoder@gmail.com>
2023-08-17 11:17:40 -0700

    updated

--- viteAdvancedTodo/src/hooks/useLocalStorage.js
+++ viteAdvancedTodo/src/hooks/useLocalStorage.js
@@ -3,20 +3,26 @@ export function useLocalStorage(key, intialValue) {
   const [value, setValue] = useState(() => {
     const localValue = localStorage.getItem(key);
     if (localValue === null) {
+      console.log("key is null");
       if (typeof intialValue === "function") {
+        console.log("its a function");
         return intialValue();
       } else {
+        console.log("its initial value");
         return intialValue;
       }
     } else {
-      return localValue;
+      console.log(localValue);
+      return JSON.parse(localValue);
     }
   });
   useEffect(() => {
     if (value === undefined) {
       localStorage.removeItem(key);
     } else {
-      localStorage.setItem(key, value);
+      localStorage.setItem(key, JSON.stringify(value));
     }
-  });
+  }, [value]);
+
+  return [value, setValue];
 }

073d4e9bd5ca98e5c766b56a587272e89aa9d070
Kyle Mitchell <kylethacoder@gmail.com>
2023-08-17 12:47:41 -0700

    updated

--- viteAdvancedTodo/src/components/TodoFilterForm.jsx
+++ viteAdvancedTodo/src/components/TodoFilterForm.jsx
@@ -1,9 +1,15 @@
-export function TodoFilterForm() {
+import "../styles/styles.css";
+export function TodoFilterForm({ name, setFilteredName }) {
   return (
     <div className="filter-form">
       <div className="filter-form-group">
         <label htmlFor="name">Name</label>
-        <input type="text" id="name" value="" />
+        <input
+          type="text"
+          id="name"
+          value={name}
+          onChange={(e) => setFilteredName(e.target.value)}
+        />
       </div>
       <label>
         <input type="checkbox" />

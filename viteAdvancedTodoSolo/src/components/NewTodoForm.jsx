32941b876cea154b05afd410103e4cba0d153d9e
Kyle Mitchell <kylethacoder@gmail.com>
2023-08-18 10:39:04 -0700

    updated

--- viteAdvancedTodoSolo/src/components/NewTodoForm.jsx
+++ viteAdvancedTodoSolo/src/components/NewTodoForm.jsx
@@ -1,14 +1,19 @@
-export function NewTodoForm({ newTodoName, setNewTodoName }) {
+import { useRef } from "react";
+
+export function NewTodoForm({ addNewTodo }) {
+  const nameRef = useRef();
+
+  function handleNewTodoSubmit(e) {
+    e.preventDefault();
+    if (nameRef.current.value === "") return;
+    addNewTodo(nameRef.current.value);
+    nameRef.current.value === "";
+  }
   return (
-    <form id="new-todo-form">
+    <form onSubmit={handleNewTodoSubmit} id="new-todo-form">
       <label htmlFor="todo-input">New Todo</label>
-      <input
-        type="text"
-        id="todo-input"
-        value={newTodoName}
-        onChange={(e) => setNewTodoName(e.target.value)}
-      />
-      <button onClick={() => addNewTodo(newTodoName)}>Add Todo</button>
+      <input type="text" id="todo-input" ref={nameRef} />
+      <button>Add Todo</button>
     </form>
   );
 }

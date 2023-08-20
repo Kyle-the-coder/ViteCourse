13bbf7866cb3c890f67f0622faa9829488fa4558
Kyle Mitchell <kylethacoder@gmail.com>
2023-08-20 09:46:53 -0700

    updated

--- viteAdvancedTodoSolo2/src/components/NewTodoForm.jsx
+++ viteAdvancedTodoSolo2/src/components/NewTodoForm.jsx
@@ -1,3 +1,15 @@
+import { useRef } from "react";
+import "../styles/styles.css";
+
 export function NewTodoForm() {
-  return <form></form>;
+  const nameRef = useRef();
+  return (
+    <form>
+      <label data-label className="newFormLabel">
+        New Todo:
+      </label>
+      <input type="text" ref={nameRef} className="newFormInput" />
+      <button>Submit</button>
+    </form>
+  );
 }

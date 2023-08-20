2605a486c4c890d5948dd14418f68f3aebe9e07f
Kyle Mitchell <kylethacoder@gmail.com>
2023-08-20 10:11:38 -0700

    updated

--- viteAdvancedTodoSolo2/src/components/TodoList.jsx
+++ viteAdvancedTodoSolo2/src/components/TodoList.jsx
@@ -1,3 +1,19 @@
+import { useContext } from "react";
+import { TodoContext } from "../App";
+
 export function TodoList() {
-  return <div></div>;
+  const { todos } = useContext(TodoContext);
+  return (
+    <div>
+      <ul>
+        {todos.map((todo) => {
+          <li>
+            <input type="checkbox" checked={todo.completed} />
+            <label>{todo.name}</label>
+            <button>delete</button>
+          </li>;
+        })}
+      </ul>
+    </div>
+  );
 }

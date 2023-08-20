c2c79ddfbabd173dd9b69fce4e912f23c927bfd7
Kyle Mitchell <kylethacoder@gmail.com>
2023-08-20 10:54:34 -0700

    updated

--- viteAdvancedTodoSolo2/src/components/TodoItem.jsx
+++ viteAdvancedTodoSolo2/src/components/TodoItem.jsx
@@ -1,3 +1,20 @@
-function TodoItem() {
-  return <div></div>;
+import { useContext } from "react";
+import { TodoContext } from "../App";
+
+export function TodoItem({ id, name, completed }) {
+  const { toggleTodo, deleteTodo } = useContext(TodoContext);
+  console.log(id);
+  return (
+    <li>
+      <label>
+        <input
+          type="checkbox"
+          checked={completed}
+          onChange={(e) => toggleTodo(id, e.target.checked)}
+        />
+        <span className={`${completed && "line"}`}>{name}</span>
+      </label>
+      <button>Delete</button>
+    </li>
+  );
 }

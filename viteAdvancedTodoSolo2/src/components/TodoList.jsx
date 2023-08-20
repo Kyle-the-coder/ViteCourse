32b8853538328484f379c6f5e06095f313633a1c
Kyle Mitchell <kylethacoder@gmail.com>
2023-08-20 10:54:37 -0700

    updated

--- viteAdvancedTodoSolo2/src/components/TodoList.jsx
+++ viteAdvancedTodoSolo2/src/components/TodoList.jsx
@@ -1,24 +1,20 @@
 import { useContext } from "react";
 import { TodoContext } from "../App";
+import { TodoItem } from "./TodoItem";
 
 export function TodoList() {
   const { todos } = useContext(TodoContext);
-  console.log(todos);
   return (
-    <div className="todoListContainer">
+    <div className="todoListParentContainer">
       <h2>List:</h2>
       <ul className="listItemContainer">
-        {todos.map((todo) => {
-          return (
-            <li>
-              <div>
-                <input type="checkbox" checked={todo.compeleted} />
-                <label>{todo.name}</label>
-                <button>Delete</button>
-              </div>
-            </li>
-          );
-        })}
+        {todos.length > 0 ? (
+          todos.map((todo) => {
+            return <TodoItem key={todo.id} {...todo} />;
+          })
+        ) : (
+          <div>add new todo</div>
+        )}
       </ul>
     </div>
   );

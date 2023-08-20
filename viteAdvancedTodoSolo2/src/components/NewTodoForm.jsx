7e552a1332e8b3b2660d91defb7c9ad596640ac7
Kyle Mitchell <kylethacoder@gmail.com>
2023-08-20 10:03:56 -0700

    updated

--- viteAdvancedTodoSolo2/src/components/NewTodoForm.jsx
+++ viteAdvancedTodoSolo2/src/components/NewTodoForm.jsx
@@ -3,13 +3,20 @@ import "../styles/styles.css";
 
 export function NewTodoForm() {
   const nameRef = useRef();
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(nameRef.current.value);
+    nameRef.current.value === "";
+  }
   return (
-    <form>
-      <label data-label className="newFormLabel">
-        New Todo:
-      </label>
-      <input type="text" ref={nameRef} className="newFormInput" />
-      <button>Submit</button>
+    <form onSubmit={handleSubmit}>
+      <div className="newFormContainer">
+        <label data-label className="newFormLabel">
+          New Todo:
+        </label>
+        <input type="text" ref={nameRef} className="newFormInput" />
+        <button className="newFormButton">Submit</button>
+      </div>
     </form>
   );
 }

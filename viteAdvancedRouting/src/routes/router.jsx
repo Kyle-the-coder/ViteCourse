c4ea7c2c01fa2233012bce5bbaefcff6304cbc8c
Kyle Mitchell <kylethacoder@gmail.com>
2023-08-21 11:35:59 -0700

    updated

--- viteAdvancedRouting/src/routes/router.jsx
+++ viteAdvancedRouting/src/routes/router.jsx
@@ -1,10 +1,18 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Outlet } from "react-router-dom";
 import { Home } from "../pages/Home";
 import { Store } from "../pages/Store";
 import { About } from "../pages/About";
+import { NavLayout } from "./layouts/NavLayout";
+import { Team } from "../pages/Team";
 
 export const router = createBrowserRouter([
-  { path: "/", element: <Home /> },
-  { path: "/store", element: <Store /> },
-  { path: "/about", element: <About /> },
+  {
+    element: <NavLayout />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/store", element: <Store /> },
+      { path: "/about", element: <About /> },
+      { path: "/team", element: <Team /> },
+    ],
+  },
 ]);

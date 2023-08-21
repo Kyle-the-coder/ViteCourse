440e60815e1f9fcef6d14885b72ab47b396f7daa
Kyle Mitchell <kylethacoder@gmail.com>
2023-08-21 10:56:55 -0700

    updated

---
+++ viteAdvancedRouting/src/Navbar.jsx
@@ -0,0 +1,15 @@
+function Navbar() {
+  return (
+    <ul>
+      <li>
+        <a href="/">Home</a>
+      </li>
+      <li>
+        <a href="/about">About</a>
+      </li>
+      <li>
+        <a href="/store">Store</a>
+      </li>
+    </ul>
+  );
+}

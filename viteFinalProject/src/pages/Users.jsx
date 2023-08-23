c111ae37e8fa357e7f8b1b147f436e4b94d0482a
Kyle Mitchell <kylethacoder@gmail.com>
2023-08-23 13:54:05 -0700

    updated

--- viteFinalProject/src/pages/Users.jsx
+++ viteFinalProject/src/pages/Users.jsx
@@ -1,25 +1,30 @@
 import { useLoaderData } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export function Users() {
   const usersInfo = useLoaderData();
-
+  console.log(usersInfo);
   return (
     <div className="container">
       <h1 className="page-title">Users</h1>
       <div className="card-grid">
-        <div className="card">
-          <div className="card-header">Leanne Graham</div>
-          <div className="card-body">
-            <div>Romaguera-Crona</div>
-            <div>hildegard.org</div>
-            <div>Sincere@april.biz</div>
-          </div>
-          <div className="card-footer">
-            <a className="btn" href="user.html">
-              View
-            </a>
-          </div>
-        </div>
+        {usersInfo.map((user) => {
+          return (
+            <div className="card" key={user.id}>
+              <div className="card-header">{user.name}</div>
+              <div className="card-body">
+                <div>{user.username}</div>
+                <div>{user.website}</div>
+                <div>{user.email}</div>
+              </div>
+              <div className="card-footer">
+                <Link className="btn" to={`/users/${user.id}`}>
+                  View
+                </Link>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );

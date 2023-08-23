0d45ea3dd2a8fa0120ffa0371d48bb3f1f52fb89
Kyle Mitchell <kylethacoder@gmail.com>
2023-08-23 12:46:53 -0700

    updated

--- viteFinalProject/src/pages/Posts.jsx
+++ viteFinalProject/src/pages/Posts.jsx
@@ -1,44 +1,24 @@
-import { Link } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
 
 export function Posts() {
+  const PostsInfo = useLoaderData();
   return (
     <div className="container">
       <h1 className="page-title">Posts</h1>
       <div className="card-grid">
-        <div className="card">
-          <div className="card-header">
-            sunt aut facere repellat provident occaecati excepturi optio
-            reprehenderit
-          </div>
-          <div className="card-body">
-            <div className="card-preview-text">
-              quia et suscipit suscipit recusandae consequuntur expedita et cum
-              reprehenderit molestiae ut ut quas totam nostrum rerum est autem
-              sunt rem eveniet architecto
+        {PostsInfo.map((post) => (
+          <div className="card" id={post.id}>
+            <div className="card-header">{post.title}</div>
+            <div className="card-body">
+              <div className="card-preview-text">{post.body}</div>
+            </div>
+            <div className="card-footer">
+              <Link className="btn" to={`/posts/${post.id}`}>
+                View
+              </Link>
             </div>
           </div>
-          <div className="card-footer">
-            <Link className="btn" to="/posts/234">
-              View
-            </Link>
-          </div>
-        </div>
-        <div className="card">
-          <div className="card-header">qui est esse</div>
-          <div className="card-body">
-            <div className="card-preview-text">
-              est rerum tempore vitae sequi sint nihil reprehenderit dolor
-              beatae ea dolores neque fugiat blanditiis voluptate porro vel
-              nihil molestiae ut reiciendis qui aperiam non debitis possimus qui
-              neque nisi nulla
-            </div>
-          </div>
-          <div className="card-footer">
-            <a className="btn" href="post.html">
-              View
-            </a>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );

a5509a8acce29eae15465b0b6e70082d5dd127a2
Kyle Mitchell <kylethacoder@gmail.com>
2023-08-23 10:56:17 -0700

    updated

---
+++ viteAdvancedRouting/src/navbars/Navbar.jsx
@@ -0,0 +1,20 @@
+import { NavLink } from "react-router-dom";
+
+export function Navbar() {
+  return (
+    <ul>
+      <li>
+        <NavLink to="/">Home</NavLink>
+      </li>
+      <li>
+        <NavLink to="/about">About</NavLink>
+      </li>
+      <li>
+        <NavLink to="/store">Store</NavLink>
+      </li>
+      <li>
+        <NavLink to="/team">Team</NavLink>
+      </li>
+    </ul>
+  );
+}

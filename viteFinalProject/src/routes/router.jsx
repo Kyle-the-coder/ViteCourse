bf6a9e5585b0311263bb54a30dc1fec1ed1ce13d
Kyle Mitchell <kylethacoder@gmail.com>
2023-08-23 11:54:51 -0700

    updated

--- viteFinalProject/src/routes/router.jsx
+++ viteFinalProject/src/routes/router.jsx
@@ -1,1 +1,14 @@
+import { createBrowserRouter } from "react-router-dom";
+import { NavLayout } from "./layouts/NavLayout";
 
+export const router = createBrowserRouter([
+  {
+    element: <NavLayout />,
+    errorElement: <h1>error</h1>,
+    children: [
+      { path: "/posts", element: <PostPage /> },
+      { path: "/users", element: <UsersPage /> },
+      { path: "/todos", element: <TodosPage /> },
+    ],
+  },
+]);

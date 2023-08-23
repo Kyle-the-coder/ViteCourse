945ab83f54709747752e9a4d05d8c7258b09c134
Kyle Mitchell <kylethacoder@gmail.com>
2023-08-23 13:31:07 -0700

    updated

--- viteFinalProject/src/pages/Posts.jsx
+++ viteFinalProject/src/pages/Posts.jsx
@@ -1,24 +1,27 @@
+import { useEffect } from "react";
 import { Link, useLoaderData } from "react-router-dom";
 
 export function Posts() {
-  const PostsInfo = useLoaderData();
+  const postsInfo = useLoaderData();
+
   return (
     <div className="container">
       <h1 className="page-title">Posts</h1>
       <div className="card-grid">
-        {PostsInfo.map((post) => (
-          <div className="card" id={post.id}>
-            <div className="card-header">{post.title}</div>
-            <div className="card-body">
-              <div className="card-preview-text">{post.body}</div>
+        {postsInfo !== undefined &&
+          postsInfo.map((post) => (
+            <div className="card" key={post.id}>
+              <div className="card-header">{post.title}</div>
+              <div className="card-body">
+                <div className="card-preview-text">{post.body}</div>
+              </div>
+              <div className="card-footer">
+                <Link className="btn" to={`/post/${post.id}`}>
+                  View
+                </Link>
+              </div>
             </div>
-            <div className="card-footer">
-              <Link className="btn" to={`/posts/${post.id}`}>
-                View
-              </Link>
-            </div>
-          </div>
-        ))}
+          ))}
       </div>
     </div>
   );

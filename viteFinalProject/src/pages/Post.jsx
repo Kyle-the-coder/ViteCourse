eef71f8dbe48e5b5e5d29cc16c36fc5ef74893ac
Kyle Mitchell <kylethacoder@gmail.com>
2023-08-23 12:46:49 -0700

    updated

--- viteFinalProject/src/pages/Post.jsx
+++ viteFinalProject/src/pages/Post.jsx
@@ -1,7 +1,37 @@
 export function Post() {
   return (
-    <>
-      <h1>post</h1>
-    </>
+    <div class="container">
+      <h1 class="page-title">
+        sunt aut facere repellat provident occaecati excepturi optio
+        reprehenderit
+      </h1>
+      <span class="page-subtitle">
+        By: <a href="user.html">Leanne Graham</a>
+      </span>
+      <div>
+        quia et suscipit suscipit recusandae consequuntur expedita et cum
+        reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt
+        rem eveniet architecto
+      </div>
+      <h3 class="mt-4 mb-2">Comments</h3>
+      <div class="card-stack">
+        <div class="card">
+          <div class="card-body">
+            <div class="text-sm mb-1">Eliseo@gardner.biz</div>
+            laudantium enim quasi est quidem magnam voluptate ipsam eos tempora
+            quo necessitatibus dolor quam autem quasi reiciendis et nam sapiente
+            accusantium
+          </div>
+        </div>
+        <div class="card">
+          <div class="card-body">
+            <div class="text-sm mb-1">Jayne_Kuhic@sydney.com</div>
+            est natus enim nihil est dolore omnis voluptatem numquam et omnis
+            occaecati quod ullam at voluptatem error expedita pariatur nihil
+            sint nostrum voluptatem reiciendis et
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }

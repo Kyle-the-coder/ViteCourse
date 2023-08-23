dbb81039e8ccbfcee38e79614c9a7e122284e5f2
Kyle Mitchell <kylethacoder@gmail.com>
2023-08-23 12:27:22 -0700

    updated

--- viteFinalProject/src/navbars/MainNavbar.jsx
+++ viteFinalProject/src/navbars/MainNavbar.jsx
@@ -2,16 +2,19 @@ import { NavLink } from "react-router-dom";
 
 export function MainNavbar() {
   return (
-    <ul>
-      <li>
-        <NavLink to="/posts">Posts</NavLink>
-      </li>
-      <li>
-        <NavLink to="/users">Users</NavLink>
-      </li>
-      <li>
-        <NavLink to="/todos">Todos</NavLink>
-      </li>
-    </ul>
+    <nav className="top-nav">
+      <div className="nav-text-large">My App</div>
+      <ul className="nav-list">
+        <li>
+          <NavLink to="/posts">Posts</NavLink>
+        </li>
+        <li>
+          <NavLink to="/users">Users</NavLink>
+        </li>
+        <li>
+          <NavLink to="/todos">Todos</NavLink>
+        </li>
+      </ul>
+    </nav>
   );
 }

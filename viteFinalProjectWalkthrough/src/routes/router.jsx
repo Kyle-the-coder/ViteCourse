e24968c41935cd9532cde9311f52ad41269a7b6f
Kyle Mitchell <kylethacoder@gmail.com>
2023-08-24 12:28:18 -0700

    updated

--- viteFinalProjectWalkthrough/src/routes/router.jsx
+++ viteFinalProjectWalkthrough/src/routes/router.jsx
@@ -1,12 +1,14 @@
-import { createBrowserRouter } from "react-router-dom";
-import { PostList, Posts } from "../pages/PostList";
+import { createBrowserRouter, Navigate } from "react-router-dom";
+import { PostList } from "../pages/PostList";
 import { TodoList } from "../pages/TodoList";
 import { UserList } from "../pages/UserList";
+import { RootLayout } from "./layouts/RootLayout";
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
     children: [
+      { index: true, element: <Navigate to="posts" /> },
       { path: "posts", element: <PostList /> },
       { path: "users", element: <UserList /> },
       { path: "todos", element: <TodoList /> },

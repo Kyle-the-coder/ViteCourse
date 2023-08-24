bd60ae40ea50f8dfce5b3e8803954b171a431334
Kyle Mitchell <kylethacoder@gmail.com>
2023-08-24 13:42:21 -0700

    updated

--- viteFinalProjectWalkthrough/src/pages/TodoList.jsx
+++ viteFinalProjectWalkthrough/src/pages/TodoList.jsx
@@ -1,3 +1,33 @@
-export function TodoList() {
-  return <>todo list</>;
+import { useLoaderData } from "react-router-dom";
+import { getTodos } from "../hooks/getTodos";
+
+function TodoList() {
+  const todos = useLoaderData();
+  console.log(todos);
+  return (
+    <>
+      <h1 className="page-title">Todos</h1>
+      <ul>
+        {todos.map((todo) => {
+          return (
+            <li
+              key={todo.id}
+              className={`${todo.completed && "strike-through"}`}
+            >
+              {todo.title}
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
 }
+
+function loader({ request: { signal } }) {
+  return getTodos({ signal });
+}
+
+export const todoListRoute = {
+  loader,
+  element: <TodoList />,
+};

bf3d0bfd6d2a96c1493947daed534bd651f046aa
Kyle Mitchell <kylethacoder@gmail.com>
2023-08-24 12:49:27 -0700

    updated

--- viteFinalProjectWalkthrough/src/pages/PostList.jsx
+++ viteFinalProjectWalkthrough/src/pages/PostList.jsx
@@ -1,9 +1,31 @@
 import axios from "axios";
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, Link } from "react-router-dom";
 
 function PostList() {
   const posts = useLoaderData();
-  return <div>Hello from posts {posts.length}</div>;
+  return (
+    <>
+      {" "}
+      <h1 className="page-title">Posts</h1>
+      <div className="card-grid">
+        {posts.map((post) => {
+          return (
+            <div className="card" key={post.id}>
+              <div className="card-header">{post.title}</div>
+              <div className="card-body">
+                <div className="card-preview-text">{post.body}</div>
+              </div>
+              <div className="card-footer">
+                <Link className="btn" to="">
+                  View
+                </Link>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
 }
 
 function loader({ request: { signal } }) {

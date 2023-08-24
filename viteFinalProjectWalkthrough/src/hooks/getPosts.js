080dd07cc73afbcfc72fffededad6f8b9bde8936
Kyle Mitchell <kylethacoder@gmail.com>
2023-08-24 13:55:56 -0700

    updated

--- viteFinalProjectWalkthrough/src/hooks/getPosts.js
+++ viteFinalProjectWalkthrough/src/hooks/getPosts.js
@@ -3,3 +3,9 @@ import { baseApi } from "./baseApi";
 export function getPosts(options) {
   return baseApi.get("posts", options).then((res) => res.data);
 }
+
+export function getPost(options) {
+  return baseApi
+    .get(`posts/${options.postId}`, options.signal)
+    .then((res) => res.data);
+}

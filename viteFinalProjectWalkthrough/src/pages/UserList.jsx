e9af759655f9e11847678e572df816645c58167a
Kyle Mitchell <kylethacoder@gmail.com>
2023-08-24 12:53:06 -0700

    updated

--- viteFinalProjectWalkthrough/src/pages/UserList.jsx
+++ viteFinalProjectWalkthrough/src/pages/UserList.jsx
@@ -1,3 +1,14 @@
-export function UserList() {
+function UserList() {
   return <>users</>;
 }
+
+function loader({ request: { signal } }) {
+  return axios
+    .get("http://localhost:3000/users", { signal })
+    .then((res) => res.data);
+}
+
+export const userListRoute = {
+  loader,
+  element: <UserList />,
+};

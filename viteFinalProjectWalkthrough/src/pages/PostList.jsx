f6c1908440b59b4156cca3047493dbb079ae3d14
Kyle Mitchell <kylethacoder@gmail.com>
2023-08-24 12:41:25 -0700

    updated

--- viteFinalProjectWalkthrough/src/pages/PostList.jsx
+++ viteFinalProjectWalkthrough/src/pages/PostList.jsx
@@ -1,3 +1,16 @@
-export function PostList() {
+import axios from "axios";
+
+function PostList() {
   return <div>Hello from posts</div>;
 }
+
+function loader({ request: { signal } }) {
+  return axios
+    .get("http://localhost:3000/posts", { signal })
+    .then((res) => res.data);
+}
+
+export const postListRoute = {
+  loader,
+  element: <PostList />,
+};

39d7b9736cd72b3431e1443e88ae883d931a6916
Kyle Mitchell <kylethacoder@gmail.com>
2023-08-24 13:07:23 -0700

    updated

--- viteFinalProjectWalkthrough/src/pages/UserList.jsx
+++ viteFinalProjectWalkthrough/src/pages/UserList.jsx
@@ -1,8 +1,32 @@
 import axios from "axios";
+import { Link } from "react-router-dom";
 import { useLoaderData } from "react-router-dom";
 function UserList() {
   const users = useLoaderData();
-  return <>users {users.length}</>;
+  return (
+    <>
+      <h1 className="page-title">Users</h1>
+      <div className="card-grid">
+        {users.map((user) => {
+          return (
+            <div className="card" key={user.id}>
+              <div className="card-header">{user.name}</div>
+              <div className="card-body">
+                <div>{user.userName}</div>
+                <div>{user.website}</div>
+                <div>{user.email}</div>
+              </div>
+              <div className="card-footer">
+                <Link className="btn" to={`${user.id}`}>
+                  View
+                </Link>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
 }
 
 function loader({ request: { signal } }) {

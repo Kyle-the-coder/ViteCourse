6db4d8cb5aa3ee0f5a4bcf2d1c2e939fd8f0acb9
Kyle Mitchell <kylethacoder@gmail.com>
2023-08-24 10:26:55 -0700

    updated

--- viteFinalProject/src/pages/Post.jsx
+++ viteFinalProject/src/pages/Post.jsx
@@ -4,11 +4,16 @@ import { useEffect, useState } from "react";
 export function Post() {
   const postInfo = useLoaderData();
   const [userInfo, setUserInfo] = useState("");
+  const [commentData, setCommentData] = useState([]);
   useEffect(() => {
     fetch(`http://127.0.0.1:3000/users/${postInfo.userId}`)
       .then((res) => res.json())
       .then((data) => setUserInfo(data));
-  }, [postInfo.userId]);
+
+    fetch(`http://127.0.0.1:3000/posts/${postInfo.id}/comments`)
+      .then((res) => res.json())
+      .then((data) => setCommentData(data));
+  }, [postInfo.userId, postInfo.id]);
   return (
     <div className="container">
       <h1 className="page-title">{postInfo.title}</h1>
@@ -18,22 +23,16 @@ export function Post() {
       <div>{postInfo.body}</div>
       <h3 className="mt-4 mb-2">Comments</h3>
       <div className="card-stack">
-        <div className="card">
-          <div className="card-body">
-            <div className="text-sm mb-1">Eliseo@gardner.biz</div>
-            laudantium enim quasi est quidem magnam voluptate ipsam eos tempora
-            quo necessitatibus dolor quam autem quasi reiciendis et nam sapiente
-            accusantium
-          </div>
-        </div>
-        <div className="card">
-          <div className="card-body">
-            <div className="text-sm mb-1">Jayne_Kuhic@sydney.com</div>
-            est natus enim nihil est dolore omnis voluptatem numquam et omnis
-            occaecati quod ullam at voluptatem error expedita pariatur nihil
-            sint nostrum voluptatem reiciendis et
-          </div>
-        </div>
+        {commentData.map((comment) => {
+          return (
+            <div className="card">
+              <div className="card-body">
+                <div className="text-sm mb-1">{comment.email}</div>
+                {comment.body}
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );

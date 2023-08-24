ec2730c485783d972e225263f1dcaf6ca2a978e5
Kyle Mitchell <kylethacoder@gmail.com>
2023-08-24 13:59:11 -0700

    updated

--- viteFinalProjectWalkthrough/src/pages/Post.jsx
+++ viteFinalProjectWalkthrough/src/pages/Post.jsx
@@ -1,13 +1,13 @@
 import { getPost } from "../hooks/getPosts";
-import { useParams } from "react-router-dom";
+import { useLoaderData } from "react-router-dom";
 
 function Post() {
-  return <h1>single post</h1>;
+  const post = useLoaderData();
+  return <h1>{post.title}</h1>;
 }
 
-function loader({ request: signal }) {
-  const postId = useParams();
-  getPost({ signal: signal, postId: postId });
+function loader({ request: signal, params }) {
+  return getPost(params.postId, { signal });
 }
 
 export const postRoute = {

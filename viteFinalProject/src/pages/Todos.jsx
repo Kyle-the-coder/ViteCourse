11f219e2974ab4f0623b2462574f68d5fc9f934a
Kyle Mitchell <kylethacoder@gmail.com>
2023-08-24 10:40:26 -0700

    updated

--- viteFinalProject/src/pages/Todos.jsx
+++ viteFinalProject/src/pages/Todos.jsx
@@ -2,14 +2,16 @@ import { useLoaderData } from "react-router-dom";
 
 export function Todos() {
   const todosData = useLoaderData();
-  console.log(todosData);
   return (
     <div className="container">
       <h1 className="page-title">Todos</h1>
       <ul>
         {todosData.map((todo) => {
           return (
-            <li className={`${todo.completed && "strike-through"} `}>
+            <li
+              className={`${todo.completed && "strike-through"} `}
+              key={todo.id}
+            >
               {todo.title}
             </li>
           );

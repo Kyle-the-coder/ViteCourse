87f9e76b4c9e805a9b27c80beb85e9e95c6bb72f
Kyle Mitchell <kylethacoder@gmail.com>
2023-08-24 14:22:29 -0700

    updated

--- viteFinalProjectWalkthrough/src/pages/User.jsx
+++ viteFinalProjectWalkthrough/src/pages/User.jsx
@@ -20,19 +20,19 @@ function User() {
       <h3 className="mt-4 mb-2">Posts</h3>
       <div className="card-grid">
         <div className="card">
-          <div class="card-header">
+          <div className="card-header">
             sunt aut facere repellat provident occaecati excepturi optio
             reprehenderit
           </div>
-          <div class="card-body">
-            <div class="card-preview-text">
+          <div className="card-body">
+            <div className="card-preview-text">
               quia et suscipit suscipit recusandae consequuntur expedita et cum
               reprehenderit molestiae ut ut quas totam nostrum rerum est autem
               sunt rem eveniet architecto
             </div>
           </div>
-          <div class="card-footer">
-            <a class="btn" href="posts.html">
+          <div className="card-footer">
+            <a className="btn" href="posts.html">
               View
             </a>
           </div>

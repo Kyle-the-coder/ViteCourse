0c76011f3a60ef2d025c799e858d95a1ef70649c
Kyle Mitchell <kylethacoder@gmail.com>
2023-08-28 11:10:53 -0700

    updated

--- viteFinalProjectWalkthrough/src/components/PostForm.jsx
+++ viteFinalProjectWalkthrough/src/components/PostForm.jsx
@@ -1,18 +1,23 @@
 import { Form, Link } from "react-router-dom";
 import { FormGroup } from "./FormGroup";
 
-export function PostForm({ users }) {
+export function PostForm({ users, defaultValues = {} }) {
   return (
     <Form method="post" className="form">
       <div className="form-row">
         <FormGroup>
           <label htmlFor="title">Title</label>
-          <input type="text" name="title" id="title" />
+          <input
+            type="text"
+            name="title"
+            id="title"
+            defaultValue={defaultValues.title}
+          />
           {/* <div className="error-message">Required</div> */}
         </FormGroup>
         <FormGroup>
-          <label for="userId">Author</label>
-          <select name="userId" id="userId">
+          <label htmlFor="userId">Author</label>
+          <select name="userId" id="userId" defaultValue={defaultValues.userId}>
             {users.map((user) => (
               <option key={user.id} value={user.id}>
                 {user.name}
@@ -23,8 +28,12 @@ export function PostForm({ users }) {
       </div>
       <div className="form-row">
         <FormGroup>
-          <label for="body">Body</label>
-          <textarea name="body" id="body"></textarea>
+          <label htmlFor="body">Body</label>
+          <textarea
+            name="body"
+            id="body"
+            defaultValue={defaultValues.body}
+          ></textarea>
         </FormGroup>
       </div>
       <div className="form-row form-btn-row">

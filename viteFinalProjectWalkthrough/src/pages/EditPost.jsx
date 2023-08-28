d86072b00313b1427da4db945c4843d7e98ac46f
Kyle Mitchell <kylethacoder@gmail.com>
2023-08-28 10:44:10 -0700

    updated

--- viteFinalProjectWalkthrough/src/pages/EditPost.jsx
+++ viteFinalProjectWalkthrough/src/pages/EditPost.jsx
@@ -1,10 +1,20 @@
+import { useLoaderData } from "react-router-dom";
 import { PostForm } from "../components/PostForm";
+import { getUsers } from "../hooks/getUsers";
 
 function EditPost() {
-  return <PostForm></PostForm>;
+  const users = useLoaderData();
+  return (
+    <>
+      <h1 className="page-title">New Post</h1>
+      <PostForm users={users} />
+    </>
+  );
 }
 
-function loader() {}
+function loader({ request: { signal } }) {
+  return getUsers({ signal });
+}
 function action() {}
 
 export const editPostRoute = {

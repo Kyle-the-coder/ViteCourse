639e91ff6d94d02ff3b23b8d8c85e858f1c3585b
Kyle Mitchell <kylethacoder@gmail.com>
2023-08-28 10:30:15 -0700

    updated

--- viteFinalProjectWalkthrough/src/pages/NewPost.jsx
+++ viteFinalProjectWalkthrough/src/pages/NewPost.jsx
@@ -1,5 +1,6 @@
 import { Form, redirect, useLoaderData } from "react-router-dom";
 import { FormGroup } from "../components/FormGroup";
+import { PostForm } from "../components/PostForm";
 import { createPost } from "../hooks/getPosts";
 import { getUsers } from "../hooks/getUsers";
 
@@ -8,37 +9,7 @@ function NewPost() {
   return (
     <>
       <h1 className="page-title">New Post</h1>
-      <Form method="post" className="form">
-        <div className="form-row">
-          <FormGroup>
-            <label htmlFor="title">Title</label>
-            <input type="text" name="title" id="title" />
-            <div className="error-message">Required</div>
-          </FormGroup>
-          <FormGroup>
-            <label for="userId">Author</label>
-            <select name="userId" id="userId">
-              {users.map((user) => (
-                <option key={user.id} value={user.id}>
-                  {user.name}
-                </option>
-              ))}
-            </select>
-          </FormGroup>
-        </div>
-        <div className="form-row">
-          <FormGroup>
-            <label for="body">Body</label>
-            <textarea name="body" id="body"></textarea>
-          </FormGroup>
-        </div>
-        <div className="form-row form-btn-row">
-          <a className="btn btn-outline" href="/posts">
-            Cancel
-          </a>
-          <button className="btn">Save</button>
-        </div>
-      </Form>
+      <PostForm users={users} />
     </>
   );
 }

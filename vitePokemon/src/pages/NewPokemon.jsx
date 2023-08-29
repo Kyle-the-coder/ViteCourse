65eaf4210efdaa4d4a13c679e97f6556c50175b2
Kyle Mitchell <kylethacoder@gmail.com>
2023-08-29 10:48:26 -0700

    updated

---
+++ vitePokemon/src/pages/NewPokemon.jsx
@@ -0,0 +1,13 @@
+function NewPokemon() {
+  return (
+    <>
+      <div className="container">
+        <h1>New</h1>
+      </div>
+    </>
+  );
+}
+
+export const newPokemonRoute = {
+  element: <NewPokemon />,
+};

c47714952bd089de58df8bc1861cc5d521e41f8a
Kyle Mitchell <kylethacoder@gmail.com>
2023-08-29 10:21:45 -0700

    updated

--- vitePokemon/src/components/Navbar.jsx
+++ vitePokemon/src/components/Navbar.jsx
@@ -1,9 +1,13 @@
 import { Link } from "react-router-dom";
+import pokeBall from "../assets/pokeball.png";
 
 export function Navbar() {
   return (
     <nav className="top-nav">
-      <div className="nav-text-large">Pokemon Storage</div>
+      <div className="nav-text-large">
+        Pokemon Storage
+        <img src={pokeBall} width="60" height="60" />
+      </div>
       <ul className="nav-list">
         <li>
           <Link to="/posts">Home</Link>

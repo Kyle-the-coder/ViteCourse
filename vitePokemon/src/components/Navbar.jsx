c98ef1b912cbb8d3b37662250aee7cd4f9318bb9
Kyle Mitchell <kylethacoder@gmail.com>
2023-08-29 10:16:03 -0700

    updated

--- vitePokemon/src/components/Navbar.jsx
+++ vitePokemon/src/components/Navbar.jsx
@@ -3,16 +3,16 @@ import { Link } from "react-router-dom";
 export function Navbar() {
   return (
     <nav className="top-nav">
-      <div className="nav-text-large">My App</div>
+      <div className="nav-text-large">Pokemon Storage</div>
       <ul className="nav-list">
         <li>
-          <Link to="/posts">Posts</Link>
+          <Link to="/posts">Home</Link>
         </li>
         <li>
-          <Link to="/users">Users</Link>
+          <Link to="/users">Your Storage</Link>
         </li>
         <li>
-          <Link to="/todos">Todos</Link>
+          <Link to="/todos">New Pokemon</Link>
         </li>
       </ul>
     </nav>

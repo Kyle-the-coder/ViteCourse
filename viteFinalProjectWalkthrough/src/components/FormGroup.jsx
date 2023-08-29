187afc1581b3dcda42a8ee747d4139c15b6b32ee
Kyle Mitchell <kylethacoder@gmail.com>
2023-08-29 09:43:27 -0700

    updated

--- viteFinalProjectWalkthrough/src/components/FormGroup.jsx
+++ viteFinalProjectWalkthrough/src/components/FormGroup.jsx
@@ -1,7 +1,7 @@
 export function FormGroup({ children, errorMessage }) {
   console.log(errorMessage);
   return (
-    <div className={`form-group ${errorMessage !== null && "error"}`}>
+    <div className={`form-group ${errorMessage !== null ? "error" : ""}`}>
       {children}
       {errorMessage !== null && <div className="error-message">Required</div>}
     </div>

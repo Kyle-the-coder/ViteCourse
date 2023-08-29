dca3b9235c0d03c61e5b04e0bdbfd49db3485bb5
Kyle Mitchell <kylethacoder@gmail.com>
2023-08-29 11:15:51 -0700

    updated

--- vitePokemon/src/pages/NewPokemon.jsx
+++ vitePokemon/src/pages/NewPokemon.jsx
@@ -1,12 +1,18 @@
-import { useLoaderData } from "react-router-dom";
+import { useRef } from "react";
+import { Form, useLoaderData } from "react-router-dom";
 import { getPokemon } from "../hooks/getPokemon";
 
 function NewPokemon() {
   const pokemon = useLoaderData();
-  console.log(pokemon);
+  const pokeNameRef = useRef();
+
   return (
     <>
       <div className="container">
+        <Form>
+          <input type="text" ref={pokeNameRef} />
+          <button>submit</button>
+        </Form>
         <h1>New</h1>
         <img src={pokemon.sprites.front_default} width="500" />
       </div>

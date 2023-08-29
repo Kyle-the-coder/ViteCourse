dc24d50e5ae4305158ba10973ccd43bdb2224041
Kyle Mitchell <kylethacoder@gmail.com>
2023-08-29 10:48:30 -0700

    updated

---
+++ vitePokemon/src/pages/Storage.jsx
@@ -0,0 +1,13 @@
+function Storage() {
+  return (
+    <>
+      <div className="container">
+        <h1>Storage</h1>
+      </div>
+    </>
+  );
+}
+
+export const storageRoute = {
+  element: <Storage />,
+};

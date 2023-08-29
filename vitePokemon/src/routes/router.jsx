6a1421e5704041d55eb67f4f5c15746f64a810d4
Kyle Mitchell <kylethacoder@gmail.com>
2023-08-29 10:48:34 -0700

    updated

--- vitePokemon/src/routes/router.jsx
+++ vitePokemon/src/routes/router.jsx
@@ -1,11 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
 import { MainLayout } from "../layouts/MainLayout";
 import { homeRoute } from "../pages/Home";
+import { newPokemonRoute } from "../pages/NewPokemon";
+import { storageRoute } from "../pages/Storage";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout />,
-    children: [{ path: "/", ...homeRoute }],
+    children: [
+      { path: "/", ...homeRoute },
+      { path: "/storage", ...storageRoute },
+      { path: "/newpokemon", ...newPokemonRoute },
+    ],
   },
 ]);

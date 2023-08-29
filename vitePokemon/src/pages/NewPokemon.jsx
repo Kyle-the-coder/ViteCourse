a6a748c0d971e80f40aefec095be3ec55f8170e3
Kyle Mitchell <kylethacoder@gmail.com>
2023-08-29 11:01:02 -0700

    updated

--- vitePokemon/src/pages/NewPokemon.jsx
+++ vitePokemon/src/pages/NewPokemon.jsx
@@ -1,13 +1,23 @@
+import { useLoaderData } from "react-router-dom";
+import { getPokemon } from "../hooks/getPokemon";
+
 function NewPokemon() {
+  const pokemon = useLoaderData();
+  console.log(pokemon);
   return (
     <>
       <div className="container">
         <h1>New</h1>
+        <img src={pokemon.sprites.front_default} width="500" />
       </div>
     </>
   );
 }
 
+function loader() {
+  return getPokemon();
+}
 export const newPokemonRoute = {
+  loader,
   element: <NewPokemon />,
 };

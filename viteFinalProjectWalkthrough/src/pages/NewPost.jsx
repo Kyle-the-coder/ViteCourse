0d405baa5e737f4009bd356df01827a0410a49a5
Kyle Mitchell <kylethacoder@gmail.com>
2023-08-29 09:20:13 -0700

    updated

--- viteFinalProjectWalkthrough/src/pages/NewPost.jsx
+++ viteFinalProjectWalkthrough/src/pages/NewPost.jsx
@@ -1,16 +1,22 @@
-import { redirect, useLoaderData, useNavigation } from "react-router-dom";
-import { PostForm } from "../components/PostForm";
+import {
+  redirect,
+  useActionData,
+  useLoaderData,
+  useNavigation,
+} from "react-router-dom";
+import { PostForm, PostFormValidator } from "../components/PostForm";
 import { createPost } from "../hooks/getPosts";
 import { getUsers } from "../hooks/getUsers";
 
 function NewPost() {
   const users = useLoaderData();
   const { state } = useNavigation();
+  const errors = useActionData();
   const isSubmitting = state === "submitting";
   return (
     <>
       <h1 className="page-title">New Post</h1>
-      <PostForm users={users} isSubmitting={isSubmitting} />
+      <PostForm users={users} isSubmitting={isSubmitting} errors={errors} />
     </>
   );
 }
@@ -20,6 +26,12 @@ async function action({ request }) {
   const title = formData.get("title");
   const body = formData.get("body");
   const userId = formData.get("userId");
+
+  const errors = PostFormValidator({ title, userId, body });
+
+  if (Object.keys(errors).length > 0) {
+    return errors;
+  }
 
   const post = await createPost(
     { title, body, userId },

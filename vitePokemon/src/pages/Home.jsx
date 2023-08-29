e4a1705a90322e277fcd61bc69e52624b188ffc7
Kyle Mitchell <kylethacoder@gmail.com>
2023-08-29 10:42:25 -0700

    updated

--- vitePokemon/src/pages/Home.jsx
+++ vitePokemon/src/pages/Home.jsx
@@ -1,5 +1,24 @@
+import pokemonStorageImg from "../assets/pokemonStorage.webp";
 function Home() {
-  return <h1>home</h1>;
+  return (
+    <>
+      <div className="container">
+        <div className="welcomeContainer">
+          <div className="welcomeTitle">
+            <h1>Welcome to Pokemon Storage!</h1>
+            <p>
+              Here you can store, view info, and search for your favorite
+              pokemon!
+            </p>
+            <button className="btn">Get Started</button>
+          </div>
+          <div className="welcomeImage">
+            <img src={pokemonStorageImg} width="400" height="200" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 function loader() {}

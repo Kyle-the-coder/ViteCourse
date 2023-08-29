471cb803c6e5fc660ac8b77d37096075b55c30e9
Kyle Mitchell <kylethacoder@gmail.com>
2023-08-29 12:30:41 -0700

    updated

--- vitePokemon/src/pages/NewPokemon.jsx
+++ vitePokemon/src/pages/NewPokemon.jsx
@@ -4,29 +4,38 @@ import { getPokemon } from "../hooks/getPokemon";
 
 function NewPokemon() {
   const pokemon = useActionData();
+  const [localOrInput, setLocalOrInput] = useState();
   const [isMounted, setIsMounted] = useState(false);
 
   useEffect(() => {
     if (pokemon !== undefined) {
       setIsMounted(true);
     }
+    const poke = localStorage.getItem("pokemon");
+
+    if (pokemon === undefined && poke.length >= 0) {
+      setIsMounted(true);
+      setLocalOrInput(JSON.parse(poke));
+    }
+    if (pokemon != undefined) {
+      setLocalOrInput(pokemon);
+    }
   }, [pokemon]);
-  console.log(pokemon);
-  console.log(isMounted);
+
   return (
     <>
       <div className="container">
         <Form method="post">
-          <input type="text" name="name" />
+          <input type="text" name="name" defaultValue={localOrInput?.name} />
           <button>submit</button>
         </Form>
         {isMounted ? (
           <div className="resultsContainer">
-            <img src={pokemon.sprites.front_default} />
-            {pokemon.name}
+            <img src={localOrInput?.sprites?.front_default} />
+            {localOrInput?.name}
           </div>
         ) : (
-          "loading"
+          <></>
         )}
       </div>
     </>
@@ -37,7 +46,9 @@ async function action({ request }) {
   const formData = await request.formData();
   const searchName = formData.get("name");
   console.log(searchName);
-  return getPokemon(searchName);
+  const pokemon = await getPokemon(searchName);
+  localStorage.setItem("pokemon", JSON.stringify(pokemon));
+  return pokemon;
 }
 
 export const newPokemonRoute = {

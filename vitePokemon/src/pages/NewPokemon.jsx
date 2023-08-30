81a9449977ff5cf6a2a9ccd18fb27dbc06446a49
Kyle Mitchell <kylethacoder@gmail.com>
2023-08-30 12:01:12 -0700

    updated

--- vitePokemon/src/pages/NewPokemon.jsx
+++ vitePokemon/src/pages/NewPokemon.jsx
@@ -3,23 +3,26 @@ import { Form, useActionData } from "react-router-dom";
 import { getPokemon } from "../hooks/getPokemon";
 
 function NewPokemon() {
-  const newPokemon = useActionData();
+  const newPokemon = localStorage.getItem("pokemon");
   const [pokemon, setPokemon] = useState(() => {
     const p = localStorage.getItem("pokemon");
-    console.log(p);
     if (p === undefined) return [];
     return JSON.parse(p);
   });
-  const [localOrInput, setLocalOrInput] = useState();
   const [isMounted, setIsMounted] = useState(false);
-  // const [pokeList, setPokeList] = useState(() => {
-  //   const list = localStorage.getItem("pokeList");
-  //   console.log(list);
-  //   if (list === undefined) return [];
-  //   return JSON.parse(list);
-  // });
+  const [pokeList, setPokeList] = useState(() => {
+    const list = localStorage.getItem("pokeList");
+    if (list === undefined) return [];
+    setIsMounted(true);
+    return JSON.parse(list);
+  });
 
-  console.log(pokemon);
+  useEffect(() => {
+    const newInfo = localStorage.getItem("pokemon");
+    console.log(newInfo);
+    setPokemon(JSON.parse(newInfo));
+  }, [newPokemon]);
+
   return (
     <>
       <div className="container">
@@ -35,19 +38,36 @@ function NewPokemon() {
         ) : (
           <></>
         )}
+        <div className="recentSearchListContainer">
+          <div>
+            <h1>Recent Searches:</h1>
+          </div>
+          {pokeList.map((pokemon) => (
+            <div key={pokemon.id}>{pokemon.name}</div>
+          ))}
+        </div>
       </div>
     </>
   );
 }
 
 async function action({ request }) {
+  const existingPokeList = localStorage.getItem("pokeList") || [];
   const formData = await request.formData();
   const searchName = formData.get("name");
   const addPokemon = await getPokemon(searchName);
-  console.log(addPokemon);
+  //HANDLE RECENT SEARCH LIST
+  if (existingPokeList.length > 0) {
+    const newPokeList = JSON.parse(existingPokeList);
+    const newList = [...newPokeList, addPokemon];
+    localStorage.setItem("pokeList", JSON.stringify(newList));
+  } else if (existingPokeList.length === 0) {
+    console.log("it equals 0");
+    existingPokeList.push(addPokemon);
+    localStorage.setItem("pokeList", JSON.stringify(existingPokeList));
+  }
+  //HANDLE CURRENT SEARCH
   localStorage.setItem("pokemon", JSON.stringify(addPokemon));
-
-  localStorage.setItem("pokeList", []);
   return addPokemon;
 }
 

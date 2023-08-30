8d4a204814a60257bdcf61077842b9cd1d08dc09
Kyle Mitchell <kylethacoder@gmail.com>
2023-08-30 13:06:41 -0700

    updated

--- vitePokemon/src/components/PokemonCard.jsx
+++ vitePokemon/src/components/PokemonCard.jsx
@@ -1,15 +1,35 @@
 import "../styles/pokemonCard.css";
+import background from "../assets/bg.webp";
+import symbol from "../assets/electricSymbol.jpeg";
 export function PokemonCard({ pokemon }) {
+  console.log(pokemon.stats[0].stat.name);
+  console.log(pokemon.stats[0].base_stat);
   return (
     <>
       {" "}
       <div className="cardContainer">
-        <div className="pokemonImgContainer">
-          <div className="pokemonImg">
-            <img src={pokemon?.sprites?.front_default} />
+        <div className="titleContainer">
+          {pokemon?.name}
+          <div>
+            {pokemon.stats[0].base_stat}
+
+            {pokemon.stats[0].stat.name}
           </div>
         </div>
-        <div className="pokemonStatsContainer">{pokemon?.name}</div>
+
+        <div className="pokemonImgContainer">
+          <div className="pokemonImg">
+            <div className="pokemonSprite">
+              <img src={pokemon?.sprites?.front_default} />
+            </div>
+            <div className="pokemonBackgroundImg">
+              <img src={background} />
+            </div>
+          </div>
+        </div>
+        <div className="pokemonStatsContainer">
+          <div className="pokemonNameContainer">{pokemon?.name}</div>
+        </div>
       </div>
     </>
   );

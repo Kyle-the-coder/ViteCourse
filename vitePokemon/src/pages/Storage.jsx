4e29d0a178a862268dd41f4daf9139654b35ec3c
Kyle Mitchell <kylethacoder@gmail.com>
2023-08-31 12:43:53 -0700

    updated

--- vitePokemon/src/pages/Storage.jsx
+++ vitePokemon/src/pages/Storage.jsx
@@ -1,8 +1,48 @@
+import { useState } from "react";
+import { useNavigation } from "react-router-dom";
+import { PokemonCard } from "../components/PokemonCard";
+
 function Storage() {
+  const { state } = useNavigation();
+  const [capturedList, setCapturedList] = useState(() => {
+    const list = localStorage.getItem("capturedList");
+    if (list === null) return [];
+    return JSON.parse(list);
+  });
+
+  function deletePokemon(pokeId) {
+    const newPokeList = capturedList.filter((id) => id.id !== pokeId);
+    localStorage.setItem("capturedList", JSON.stringify(newPokeList));
+    const newInfo = localStorage.getItem("capturedList");
+    setCapturedList(JSON.parse(newInfo));
+  }
   return (
     <>
       <div className="container">
         <h1>Storage</h1>
+        <div className="card-grid">
+          {capturedList
+            ?.slice()
+            .reverse()
+            .map((pokemon) => {
+              return (
+                <div className="gridContainer" key={pokemon.id}>
+                  <PokemonCard
+                    key={pokemon.id}
+                    pokemon={pokemon}
+                    state={state}
+                  />
+
+                  <button
+                    className="btn"
+                    onClick={() => deletePokemon(pokemon.id)}
+                  >
+                    Delete
+                  </button>
+                </div>
+              );
+            })}
+        </div>
       </div>
     </>
   );

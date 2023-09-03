a166bc0e1606f4db560f9ab785a945bc2c9f2ecd
Kyle Mitchell <kylethacoder@gmail.com>
2023-09-03 09:03:00 -0700

    updated

--- vitePokemon/src/components/NotFoundCard.jsx
+++ vitePokemon/src/components/NotFoundCard.jsx
@@ -1,3 +1,37 @@
 export function NotFoundCard() {
-  return <></>;
+  return (
+    <>
+      <div className="cardContainer">
+        <div className="titleContainer">
+          <p>Pokemon Name</p>
+          <div>Stats</div>
+        </div>
+
+        <div className="pokemonImgContainer">
+          <div className="pokemonImg">
+            <div className="pokemonSprite"></div>
+            <div className="pokemonBackgroundImg">
+              <img src={background} />
+            </div>
+          </div>
+        </div>
+
+        <div className="pokemonStatsContainer">
+          <div className="pokemonNameContainer">
+            <div className="pokemonMovesContainer">
+              <div>Move 1</div>
+              <div>Move 2</div>
+            </div>
+            <div className="pokedexLink">
+              <button disabled={true} className="pokedexBtn">
+                Pokedex
+              </button>
+
+              <img src={pokeBallEmpty} width="40" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }

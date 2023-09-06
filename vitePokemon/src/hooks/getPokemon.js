5b65cb51b01a6ef28d309ddba4bbad2780de46e3
Kyle Mitchell <kylethacoder@gmail.com>
2023-09-06 11:28:36 -0700

    updated

--- vitePokemon/src/hooks/getPokemon.js
+++
@@ -1,9 +0,0 @@
-import { EmptyCard } from "../components/EmptyCard";
-import { baseApi } from "./baseApi";
-
-export function getPokemon(input) {
-  return baseApi
-    .get(`pokemon/${input}`)
-    .then((res) => res.data)
-    .catch((err) => console.log(err));
-}

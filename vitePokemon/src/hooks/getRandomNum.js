39c4393cd275dc6b2f2a4bacb5e129c5a2671c22
Kyle Mitchell <kylethacoder@gmail.com>
2023-09-06 11:28:40 -0700

    updated

--- vitePokemon/src/hooks/getRandomNum.js
+++
@@ -1,8 +0,0 @@
-export function getRandomNum() {
-  const min = 1; // Minimum value (inclusive)
-  const max = 10; // Maximum value (inclusive)
-
-  const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
-
-  return randomNumber;
-}

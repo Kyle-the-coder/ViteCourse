181e5e9096bd4d1d8c54ff20f4ac4d95f766522c
Kyle Mitchell <kylethacoder@gmail.com>
2023-09-06 11:39:04 -0700

    updated

--- vitePokemon/src/pages/NewPokemon.jsx
+++ vitePokemon/src/pages/NewPokemon.jsx
@@ -6,6 +6,7 @@ import { PokemonCard } from "../components/PokemonCard";
 import { getPokemon } from "../api/getPokemon";
 import { getRandomNum } from "../api/getRandomNum";
 import { v4 as uuidv4 } from "uuid";
+import { handleCapture } from "../functions/handleCapture";
 
 function NewPokemon() {
   const [isCaptured, setIsCaptured] = useState(false);
@@ -48,63 +49,12 @@ function NewPokemon() {
     setPokemon(getInfo);
   }
 
-  function handleCapture(pokeInfo) {
-    //FALSEY INPUT
-    if (!pokemon.captured) {
-      setIsBallThrown(true);
-      const rand = getRandomNum();
-      const runRand = getRandomNum();
-      console.log(runRand);
-      setTimeout(() => {
-        if (runRand >= 3) {
-          if (rand >= 7) {
-            //HANDLE SINGLE POKEMON UPDATE
-            const pokemon = JSON.parse(localStorage.getItem("pokemon"));
-            pokemon.captured = true;
-            localStorage.setItem("pokemon", JSON.stringify(pokemon));
-            //HANDLE POKE LIST UPDATE
-            const existingPokeList = localStorage.getItem("pokeList") || [];
-            const newPokeList = JSON.parse(existingPokeList);
-            const changeCapture = newPokeList.map((poke) => {
-              if (poke.key === pokemon.key) {
-                return { ...poke, captured: true };
-              } else {
-                return { ...poke };
-              }
-            });
-
-            setIsBallThrown(false);
-            localStorage.setItem("pokeList", JSON.stringify(changeCapture));
-          } else if (rand < 7) {
-            console.log("did not capture");
-            setIsBallThrown(false);
-            return;
-          }
-          setIsBallThrown(false);
-        } else if (runRand < 3) {
-          return handleRun();
-        }
-      }, [2000]);
-
-      //TRUTHY INPUT
-    } else if (pokemon.captured) {
-      //HANDLE SINGLE POKEMON UPDATE
-      localStorage.setItem("capturedInfo", JSON.stringify([]));
-      const pokemon = JSON.parse(localStorage.getItem("pokemon"));
-      pokemon.captured = false;
-      localStorage.setItem("pokemon", JSON.stringify(pokemon));
-      //HANDLE POKELIST UPDATE
-      const existingPokeList = localStorage.getItem("pokeList");
-      const newPokeList = JSON.parse(existingPokeList);
-      const changeCapture = newPokeList.map((poke) => {
-        if (poke.pokeInfo.id === pokeInfo.id) {
-          return { ...poke, captured: false };
-        } else {
-          return { ...poke };
-        }
-      });
-      localStorage.setItem("pokeList", JSON.stringify(changeCapture));
-    }
+  function handleBallThrown(pokeInfo) {
+    setIsBallThrown(true);
+    setTimeout(() => {
+      handleCapture(pokeInfo);
+      setIsBallThrown(false);
+    }, [2000]);
   }
 
   return (
@@ -169,7 +119,7 @@ function NewPokemon() {
                   <button
                     onClick={() => {
                       setIsCaptured(!isCaptured);
-                      handleCapture(JSON.parse(pokemon.pokeInfo));
+                      handleBallThrown(JSON.parse(pokemon.pokeInfo));
                     }}
                     className="btn"
                     disabled={pokemon.captured}

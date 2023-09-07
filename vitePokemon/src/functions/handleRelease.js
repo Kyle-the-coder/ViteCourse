a64177e6597cb549fd7c3e8f5c0690ca67792dc6
Kyle Mitchell <kylethacoder@gmail.com>
2023-09-07 11:06:46 -0700

    updated

--- vitePokemon/src/functions/handleRelease.js
+++ vitePokemon/src/functions/handleRelease.js
@@ -1,5 +1,8 @@
-export function handleRelease(pokeId) {
-  const newPokeList = pokeList.filter((id) => id.id !== pokeId);
+export function handleRelease(pokeKey) {
+  console.log(pokeKey);
+  const pokeList = JSON.parse(localStorage.getItem("pokeList"));
+  console.log(pokeList);
+  const newPokeList = pokeList.filter((key) => key.key !== pokeKey);
   localStorage.setItem("pokeList", JSON.stringify(newPokeList));
   const newInfo = localStorage.getItem("pokeList");
   return newInfo;

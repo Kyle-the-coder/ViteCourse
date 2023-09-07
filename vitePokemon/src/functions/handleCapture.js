abdc2050f93da7b9e0e82192e223716210306386
Kyle Mitchell <kylethacoder@gmail.com>
2023-09-07 11:06:42 -0700

    updated

--- vitePokemon/src/functions/handleCapture.js
+++ vitePokemon/src/functions/handleCapture.js
@@ -1,4 +1,5 @@
 import { getRandomNum } from "../api/getRandomNum";
+import { v4 as uuidv4 } from "uuid";
 
 export function handleRun(pokemon) {
   localStorage.setItem("pokemon", null);
@@ -7,35 +8,47 @@ export function handleRun(pokemon) {
   return (pokemon = getInfo);
 }
 export function handleCapture(pokemon) {
+  const randomUUID = uuidv4();
   //FALSEY INPUT
   if (!pokemon.captured) {
     const rand = getRandomNum();
     const runRand = getRandomNum();
 
-    if (runRand >= 4) {
-      if (rand >= 7) {
+    if (runRand >= 2) {
+      if (rand >= 4) {
         //HANDLE SINGLE POKEMON UPDATE
         const pokemon = JSON.parse(localStorage.getItem("pokemon"));
         pokemon.captured = true;
         localStorage.setItem("pokemon", JSON.stringify(pokemon));
         //HANDLE POKE LIST UPDATE
         const existingPokeList = localStorage.getItem("pokeList") || [];
-        const newPokeList = JSON.parse(existingPokeList);
-        const changeCapture = newPokeList.map((poke) => {
-          if (poke.key === pokemon.key) {
-            return { ...poke, captured: true };
-          } else {
-            return { ...poke };
-          }
-        });
 
-        localStorage.setItem("pokeList", JSON.stringify(changeCapture));
-      } else if (rand < 7) {
+        if (existingPokeList.length === 0) {
+          const newPokeList = [
+            {
+              pokeInfo: pokemon.pokeInfo,
+              captured: true,
+              key: randomUUID,
+              shiny: pokemon.shiny,
+            },
+          ];
+          localStorage.setItem("pokeList", JSON.stringify(newPokeList));
+        } else if (existingPokeList.length !== 0) {
+          const newPokeList = JSON.parse(existingPokeList);
+          newPokeList.push({
+            pokeInfo: pokemon.pokeInfo,
+            captured: true,
+            key: randomUUID,
+            shiny: pokemon.shiny,
+          });
+          localStorage.setItem("pokeList", JSON.stringify(newPokeList));
+        }
+      } else if (rand < 4) {
         console.log("did not capture");
 
         return;
       }
-    } else if (runRand < 4) {
+    } else if (runRand < 2) {
       return handleRun();
     }
 

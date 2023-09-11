932d7f96525c08a254a605e97cf7f1fb983e6aa3
Kyle Mitchell <kylethacoder@gmail.com>
2023-09-11 11:07:26 -0700

    updated

--- vitePokemon/src/components/PokeList.jsx
+++ vitePokemon/src/components/PokeList.jsx
@@ -11,7 +11,7 @@ export function PokeList({
 }) {
   const { state } = useNavigation();
   const [isBallThrown, setIsBallThrown] = useState(false);
-  console.log(pokeList.map((pokemon) => pokemon.captured.capture));
+
   return (
     <>
       {" "}

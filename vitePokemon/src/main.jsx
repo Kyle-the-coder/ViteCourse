30d7665fd6035baf64bf898c47a9e7aaa72e685d
Kyle Mitchell <kylethacoder@gmail.com>
2023-09-13 12:17:20 -0700

    updated

--- vitePokemon/src/main.jsx
+++ vitePokemon/src/main.jsx
@@ -3,8 +3,6 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/router";
 import "./styles/styles.css";
-
-localStorage.setItem("pokeballCount", 10);
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />

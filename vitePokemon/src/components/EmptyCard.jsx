d64fb37c4c3d97450cdc5dcd00db7a671aef4b2a
Kyle Mitchell <kylethacoder@gmail.com>
2023-09-13 12:13:19 -0700

    updated

--- vitePokemon/src/components/EmptyCard.jsx
+++ vitePokemon/src/components/EmptyCard.jsx
@@ -1,40 +1,13 @@
 import background from "../assets/bg.webp";
 import pokeBallEmpty from "../assets/pokeballEmpty.png";
-import "../styles/pokemonCard.css";
+import cardBack from "../assets/pokemonBackCard.jpeg";
+import "../styles/cardBack.css";
 
 export function EmptyCard() {
   return (
     <>
-      <div className="cardContainer">
-        <div className="titleContainer">
-          <p>Pokemon Name</p>
-          <div>Stats</div>
-        </div>
-
-        <div className="pokemonImgContainer">
-          <div className="pokemonImg">
-            <div className="pokemonSprite"></div>
-            <div className="pokemonBackgroundImg">
-              <img src={background} />
-            </div>
-          </div>
-        </div>
-
-        <div className="pokemonStatsContainer">
-          <div className="pokemonNameContainer">
-            <div className="pokemonMovesContainer">
-              <div>Move 1</div>
-              <div>Move 2</div>
-            </div>
-            <div className="pokedexLink">
-              <button disabled={true} className="pokedexBtn">
-                Pokedex
-              </button>
-
-              <img src={pokeBallEmpty} width="40" />
-            </div>
-          </div>
-        </div>
+      <div className="emptyCardContainer">
+        <img className="cardBackImg" src={cardBack} />
       </div>
     </>
   );

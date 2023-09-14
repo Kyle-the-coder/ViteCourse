c44fca332d28b400f00f66dd80aaa416d57336a0
Kyle Mitchell <kylethacoder@gmail.com>
2023-09-14 11:33:05 -0700

    updated

--- vitePokemon/src/pages/NewPokemon.jsx
+++ vitePokemon/src/pages/NewPokemon.jsx
@@ -148,10 +148,19 @@ function NewPokemon() {
                 <>
                   <div>{pokemon.shiny ? <h1>Wow!</h1> : ""}</div>
                   <h1>
-                    You Found a wild {pokemon.shiny ? "Shiny " : ""}
-                    {pokemon?.pokeInfo.name.charAt(0).toUpperCase() +
-                      pokemon?.pokeInfo.name.slice(1).toLowerCase()}
-                    !
+                    {pokemon.captured.capture === true
+                      ? `You Caught a wild ${pokemon.shiny ? "Shiny " : ""}
+                    ${
+                      pokemon?.pokeInfo.name.charAt(0).toUpperCase() +
+                      pokemon?.pokeInfo.name.slice(1).toLowerCase()
+                    }
+                    ! `
+                      : `You Found a wild ${pokemon.shiny ? "Shiny " : ""}
+                    ${
+                      pokemon?.pokeInfo.name.charAt(0).toUpperCase() +
+                      pokemon?.pokeInfo.name.slice(1).toLowerCase()
+                    }
+                    ! `}
                   </h1>
                 </>
               )}

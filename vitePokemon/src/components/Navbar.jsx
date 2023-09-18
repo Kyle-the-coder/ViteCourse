1025156efcdae4d6093717e840da61eb174df636
Kyle Mitchell <kylethacoder@gmail.com>
2023-09-18 11:25:05 -0700

    updated

--- vitePokemon/src/components/Navbar.jsx
+++ vitePokemon/src/components/Navbar.jsx
@@ -1,7 +1,14 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import pokeBall from "../assets/pokeball.png";
 
 export function Navbar() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+    document.body.classList.toggle("dark-mode", darkMode);
+  };
   return (
     <nav className="top-nav">
       <div className="nav-text-large">
@@ -18,6 +25,7 @@ export function Navbar() {
           <Link to="/newpokemon">New Pokemon</Link>
         </li>
       </ul>
+      <button onClick={toggleDarkMode}>Toggle Dark Mode</button>
     </nav>
   );
 }

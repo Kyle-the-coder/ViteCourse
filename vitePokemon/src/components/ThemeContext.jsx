ed2751641506a265557b81810488b0cb8451afea
Kyle Mitchell <kylethacoder@gmail.com>
2023-09-18 12:58:29 -0700

    updated

--- vitePokemon/src/components/ThemeContext.jsx
+++ vitePokemon/src/components/ThemeContext.jsx
@@ -4,26 +4,38 @@ import React, { createContext, useContext, useEffect, useState } from "react";
 const ThemeContext = createContext();
 
 export function ThemeProvider({ children }) {
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useState(() => {
+    const isDark = localStorage.getItem("isDarkMode");
+    if (isDark == null) return false;
+    console.log(isDark);
+    return JSON.parse(isDark);
+  });
 
   useEffect(() => {
-    localStorage.setItem("isDarkMode", darkMode);
-  }, []);
+    if (darkMode) {
+      document.body.classList.add("dark-mode");
+    } else if (!darkMode) {
+      document.body.classList.remove("dark-mode");
+    }
+  }, [darkMode]);
 
   const toggleDarkMode = () => {
-    document.body.classList.toggle("dark-mode", darkMode);
-    localStorage.setItem("isDarkMode", darkMode);
-    const isDark = localStorage.getItem("isDarkMode");
+    if (!darkMode) {
+      document.body.classList.remove("dark-mode");
+    } else if (darkMode) {
+      document.body.classList.add("dark-mode");
+    }
     setDarkMode(!darkMode);
+    localStorage.setItem("isDarkMode", !darkMode);
   };
 
+  console.log("them", darkMode);
   return (
     <ThemeContext.Provider value={{ darkMode, toggleDarkMode }}>
       {children}
     </ThemeContext.Provider>
   );
 }
-
 export function useTheme() {
   return useContext(ThemeContext);
 }

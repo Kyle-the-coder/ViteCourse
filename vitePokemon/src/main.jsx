2b2ff717e5f53b49fea5d3788b0dd17727f0a6aa
Kyle Mitchell <kylethacoder@gmail.com>
2023-09-18 11:25:01 -0700

    updated

--- vitePokemon/src/main.jsx
+++ vitePokemon/src/main.jsx
@@ -5,18 +5,9 @@ import { router } from "./routes/router";
 import "./styles/styles.css";
 
 function Main() {
-  const [darkMode, setDarkMode] = useState(false);
-
-  const toggleDarkMode = () => {
-    setDarkMode(!darkMode);
-    document.body.classList.toggle("dark-mode", darkMode);
-  };
-
   return (
     <React.StrictMode>
-      <RouterProvider router={router}>
-        <button onClick={toggleDarkMode}>Toggle Dark Mode</button>
-      </RouterProvider>
+      <RouterProvider router={router} />
     </React.StrictMode>
   );
 }

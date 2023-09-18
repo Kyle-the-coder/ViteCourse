1d84ba0d42f3c52431bff42cd0016170385c71a7
Kyle Mitchell <kylethacoder@gmail.com>
2023-09-18 11:42:05 -0700

    updated

---
+++ vitePokemon/src/components/ThemeContext.jsx
@@ -0,0 +1,23 @@
+// ThemeContext.js
+import React, { createContext, useContext, useState } from "react";
+
+const ThemeContext = createContext();
+
+export function ThemeProvider({ children }) {
+  const [darkMode, setDarkMode] = useState(true);
+
+  const toggleDarkMode = () => {
+    document.body.classList.toggle("dark-mode", darkMode);
+    setDarkMode(!darkMode);
+  };
+
+  return (
+    <ThemeContext.Provider value={{ darkMode, toggleDarkMode }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+}
+
+export function useTheme() {
+  return useContext(ThemeContext);
+}

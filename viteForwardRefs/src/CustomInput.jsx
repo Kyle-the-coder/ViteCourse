09f7a1b297d13bca51b1c79b7bac881b258d6d1c
Kyle Mitchell <kylethacoder@gmail.com>
2023-09-21 10:49:12 -0700

    updated

--- viteForwardRefs/src/CustomInput.jsx
+++ viteForwardRefs/src/CustomInput.jsx
@@ -1,7 +1,7 @@
 import { forwardRef } from "react";
 
-function InnerCustomInput(props) {
-  return <input {...props} style={{ border: "2px solid green" }} />;
+function InnerCustomInput(props, ref) {
+  return <input {...props} ref={ref} style={{ border: "2px solid green" }} />;
 }
 
 export const CustomInput = forwardRef(InnerCustomInput);

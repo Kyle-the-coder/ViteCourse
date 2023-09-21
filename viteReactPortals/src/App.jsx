cfa8ea178337973a8db79d1a6386a8285c89ebdc
Kyle Mitchell <kylethacoder@gmail.com>
2023-09-21 10:24:32 -0700

    updated

--- viteReactPortals/src/App.jsx
+++ viteReactPortals/src/App.jsx
@@ -4,7 +4,7 @@ import { createPortal } from "react-dom";
 function App() {
   const [isOpen, setIsOpen] = useState(false);
   return (
-    <div>
+    <div style={{ position: "relative", marginTop: "100px" }}>
       <h1>app content</h1>
       <button onClick={() => setIsOpen(true)}>Show Message</button>
       <AlertMessage isOpen={isOpen} onClose={() => setIsOpen(false)}>
@@ -19,7 +19,17 @@ function App() {
 function AlertMessage({ children, onClose, isOpen }) {
   if (!isOpen) return null;
   return createPortal(
-    <div onClick={onClose} style={{ backgroundColor: "blue" }}>
+    <div
+      onClick={onClose}
+      style={{
+        backgroundColor: "blue",
+        cursor: "pointer",
+        position: "absolute",
+        top: ".5rem",
+        left: "50%",
+        translate: "-50%",
+      }}
+    >
       {children}
     </div>,
     document.querySelector("#alertMessages")

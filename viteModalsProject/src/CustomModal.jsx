37fa7b970ff573f68466482c6ed50165305b1a89
Kyle Mitchell <kylethacoder@gmail.com>
2023-09-22 09:55:08 -0700

    updated

--- viteModalsProject/src/CustomModal.jsx
+++ viteModalsProject/src/CustomModal.jsx
@@ -1,14 +1,23 @@
+import { Children, useEffect } from "react";
 import { createPortal } from "react-dom";
 
-export function CustomModal({ isCustomShown, setIsCustomShown }) {
+export function CustomModal({ isCustomShown, setIsCustomShown, children }) {
+  useEffect(() => {
+    function handler(e) {
+      if (e.key === "Escape") {
+        setIsCustomShown(false);
+      }
+    }
+
+    document.addEventListener("keydown", handler);
+
+    return () => {
+      document.removeEventListener("keydown", handler);
+    };
+  }, [isCustomShown]);
   return createPortal(
     <div className={`modal-overlay ${isCustomShown && "show"}`}>
-      <div className="modal">
-        <p>
-          This is a <strong>CUSTOM</strong> modal
-        </p>
-        <button onClick={() => setIsCustomShown(false)}>Close</button>
-      </div>
+      <div className="modal">{children}</div>
     </div>,
     document.querySelector("#modalSection")
   );

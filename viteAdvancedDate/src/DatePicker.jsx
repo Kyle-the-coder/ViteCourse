cc9150de1d0df4b29cbfb4f3d75f73973968b89d
Kyle Mitchell <kylethacoder@gmail.com>
2023-09-22 11:15:24 -0700

    updated

--- viteAdvancedDate/src/DatePicker.jsx
+++ viteAdvancedDate/src/DatePicker.jsx
@@ -1,9 +1,24 @@
-export function DatePicker({ setCurrentDate }) {
+import {
+  eachDayOfInterval,
+  startOfMonth,
+  endOfMonth,
+  format,
+  parseISO,
+} from "date-fns";
+
+export function DatePicker({ currentDate, setCurrentDate }) {
+  const currentMonthDateFormat = format(currentDate, "MMMM - yyyy");
+  const year = format(currentDate, "yyyy");
+  const month = format(currentDate, "MM");
+  const startDate = startOfMonth(new Date(year, month - 1, 1));
+  const endDate = endOfMonth(new Date(year, month - 1, 1));
+  const daysInMonth = eachDayOfInterval({ start: startDate, end: endDate });
+
   return (
     <div className="date-picker">
       <div className="date-picker-header">
         <button className="prev-month-button month-button">&larr;</button>
-        <div className="current-month">June - 2023</div>
+        <div className="current-month">{currentMonthDateFormat}</div>
         <button className="next-month-button month-button">&rarr;</button>
       </div>
       <div className="date-picker-grid-header date-picker-grid">
@@ -16,41 +31,18 @@ export function DatePicker({ setCurrentDate }) {
         <div>Sat</div>
       </div>
       <div className="date-picker-grid-dates date-picker-grid">
-        <button className="date date-picker-other-month-date">28</button>
-        <button className="date date-picker-other-month-date">29</button>
-        <button className="date date-picker-other-month-date">30</button>
-        <button className="date date-picker-other-month-date">31</button>
-        <button className="date">1</button>
-        <button className="date">2</button>
-        <button className="date">3</button>
-        <button className="date">4</button>
-        <button className="date">5</button>
-        <button className="date">6</button>
-        <button className="date">7</button>
-        <button className="date">8</button>
-        <button className="date">9</button>
-        <button className="date">10</button>
-        <button className="date">11</button>
-        <button className="date">12</button>
-        <button className="date">13</button>
-        <button className="date">14</button>
-        <button className="date">15</button>
-        <button className="date">16</button>
-        <button className="date">17</button>
-        <button className="date">18</button>
-        <button className="date">19</button>
-        <button className="date">20</button>
-        <button className="date">21</button>
-        <button className="date">22</button>
-        <button className="date">23</button>
-        <button className="date">24</button>
-        <button className="date">25</button>
-        <button className="date selected">26</button>
-        <button className="date">27</button>
-        <button className="date">28</button>
-        <button className="date today">29</button>
-        <button className="date">30</button>
-        <button className="date date-picker-other-month-date">1</button>
+        {daysInMonth.map((date) => {
+          const dayOfMonth = date.getDate();
+          return (
+            <button
+              key={dayOfMonth}
+              className="date"
+              onClick={() => setCurrentDate(date)}
+            >
+              {dayOfMonth}
+            </button>
+          );
+        })}
       </div>
     </div>
   );

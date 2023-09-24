45f4346f7b49ea62521777f6cc91440e5964945b
Kyle Mitchell <kylethacoder@gmail.com>
2023-09-24 11:35:11 -0700

    updated

--- viteUseImperativeHandle/src/Input.jsx
+++ viteUseImperativeHandle/src/Input.jsx
@@ -1,7 +1,21 @@
-import { forwardRef } from "react";
+import { forwardRef, useImperativeHandle, useRef } from "react";
 
 function Inner(props, ref) {
-  return <input {...props} ref={ref} />;
+  const [value, setValue] = useState();
+  const inputRef = useRef();
+  const input2Ref = useRef();
+  useImperativeHandle(ref, () => {
+    return { input1: inputRef.current, input2: input2Ref.current };
+  });
+  return (
+    <>
+      <input
+        type="text"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+    </>
+  );
 }
 
 export const Input = forwardRef(Inner);

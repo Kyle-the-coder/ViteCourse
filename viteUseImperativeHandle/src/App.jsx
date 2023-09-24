b499d2d3cd074a4e3a0466180c47bb3fff59a352
Kyle Mitchell <kylethacoder@gmail.com>
2023-09-24 11:35:07 -0700

    updated

--- viteUseImperativeHandle/src/App.jsx
+++ viteUseImperativeHandle/src/App.jsx
@@ -1,10 +1,12 @@
-import { useRef, useState } from "react";
+import { useRef } from "react";
+import { Input } from "./Input";
 
 function App() {
   const inputRef = useRef();
+
   return (
     <>
-      <button onClick={() => inputRef.current.focus()}>Focus</button>
+      <button onClick={() => inputRef.current.input2.focus()}>Focus</button>
       <Input type="text" ref={inputRef} />
     </>
   );

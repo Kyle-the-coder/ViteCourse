6c8491a7632533b59a55a0db2f4058239d8c103c
Kyle Mitchell <kylethacoder@gmail.com>
2023-09-24 12:35:59 -0700

    updated

--- viteInfiniteScroll/src/App.jsx
+++ viteInfiniteScroll/src/App.jsx
@@ -1,7 +1,34 @@
 import { useState } from "react";
+import "./styles.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="cardContainer">
+        <div className="card">This is first Card</div>
+        <div className="card">Card</div>
+        <div className="card">Card</div>
+        <div className="card">Card</div>
+        <div className="card">Card</div>
+        <div className="card">Card</div>
+        <div className="card">Card</div>
+        <div className="card">Card</div>
+        <div className="card">Card</div>
+        <div className="card">Card</div>
+        <div className="card">Card</div>
+        <div className="card">Card</div>
+        <div className="card">Card</div>
+        <div className="card">Card</div>
+        <div className="card">Card</div>
+        <div className="card">Card</div>
+        <div className="card">Card</div>
+        <div className="card">Card</div>
+        <div className="card">Card</div>
+        <div className="card">Card</div>
+        <div className="card">This is last Card</div>
+      </div>
+    </>
+  );
 }
 
 export default App;

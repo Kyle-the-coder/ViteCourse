4bf6b17c2bbffa20c252aea1e33a3324c2a0800a
Kyle Mitchell <kylethacoder@gmail.com>
2023-09-25 10:38:04 -0700

    updated

--- viteInfiniteScroll/src/App.jsx
+++ viteInfiniteScroll/src/App.jsx
@@ -1,10 +1,55 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./styles.css";
 
 function App() {
+  const cardContainerRef = useRef(null);
+
+  useEffect(() => {
+    const cards = cardContainerRef.current.querySelectorAll(".card");
+
+    const observer = new IntersectionObserver(
+      (entries) => {
+        console.log(entries);
+        entries.forEach((entry) => {
+          entry.target.classList.toggle("show", entry.isIntersecting);
+        });
+      },
+      { threshold: 1 }
+    );
+
+    cards.forEach((card) => {
+      observer.observe(card);
+    });
+
+    const lastCardObserver = new IntersectionObserver(
+      (entries) => {
+        const lastCard = entries[0];
+        if (!lastCard.isIntersecting) return;
+        loadNewCards();
+        lastCardObserver.unobserve(lastCard.target);
+        lastCardObserver.observe(
+          cardContainerRef.current.querySelector(".card:last-child")
+        );
+      },
+      { threshold: 1 }
+    );
+
+    lastCardObserver.observe(
+      cardContainerRef.current.querySelector(".card:last-child")
+    );
+  }, []);
+
+  function loadNewCards() {
+    for (let i = 0; i < 10; i++) {
+      const card = document.createElement("div");
+      card.textContent = "New Card";
+      card.className = "card";
+      cardContainerRef.current.append(card);
+    }
+  }
   return (
     <>
-      <div className="cardContainer">
+      <div ref={cardContainerRef} className="cardContainer">
         <div className="card">This is first Card</div>
         <div className="card">Card</div>
         <div className="card">Card</div>
@@ -30,19 +75,5 @@ function App() {
     </>
   );
 }
-
-const cards = document.querySelectorAll(".card");
-
-const observer = new IntersectionObserver(
-  (entries) => {
-    entries.forEach((entry) => {
-      entry.target.classList.toggle("show", entry.isIntersecting);
-    });
-  },
-  { threshold: 1 }
-);
-cards.forEach((card) => {
-  observer.observe(card);
-});
 
 export default App;

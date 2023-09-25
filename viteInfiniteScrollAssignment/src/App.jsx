55debbd5b6add13848585b4e00e830f47c194469
Kyle Mitchell <kylethacoder@gmail.com>
2023-09-25 10:53:05 -0700

    updated

--- viteInfiniteScrollAssignment/src/App.jsx
+++ viteInfiniteScrollAssignment/src/App.jsx
@@ -1,7 +1,25 @@
 import { useState } from "react";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      {" "}
+      <div class="grid">
+        <img src="https://via.placeholder.com/600/92c952" />
+        <img src="https://via.placeholder.com/600/771796" />
+        <img src="https://via.placeholder.com/600/24f355" />
+        <img src="https://via.placeholder.com/600/d32776" />
+        <img src="https://via.placeholder.com/600/f66b97" />
+        <img src="https://via.placeholder.com/600/92c952" />
+        <div class="skeleton">Loading...</div>
+        <div class="skeleton">Loading...</div>
+        <div class="skeleton">Loading...</div>
+        <div class="skeleton">Loading...</div>
+        <div class="skeleton">Loading...</div>
+        <div class="skeleton">Loading...</div>
+      </div>
+    </>
+  );
 }
 
 export default App;

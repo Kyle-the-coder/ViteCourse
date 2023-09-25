86fa12f32f707952d3c5c020be10698267212b13
Kyle Mitchell <kylethacoder@gmail.com>
2023-09-25 12:41:55 -0700

    updated

--- viteInfiniteScrollAssignment/src/App.jsx
+++ viteInfiniteScrollAssignment/src/App.jsx
@@ -1,41 +1,76 @@
-import { useEffect, useState } from "react";
-
+import { Fragment, useCallback, useEffect, useRef, useState } from "react";
 import axios from "axios";
 
 function App() {
-  const [results, setResults] = useState(null);
   const [page, setPage] = useState(1);
+  const gridRef = useRef();
+  const [results, setResults] = useState(null);
+  const getColorSquares = useCallback(() => {
+    const lastCard = gridRef.current.querySelector(".last");
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            setPage((prevPage) => prevPage + 1);
+            const stringPage = (page + 1).toString();
+            console.log("inside", page + 1);
+            loadNewCards(stringPage);
+            observer.unobserve(lastCard);
+          }
+        });
+      },
+      { rootMargin: "-100px" }
+    );
+    observer.observe(lastCard);
+  }, [page]);
+
+  function loadNewCards(stringPage) {
+    axios
+      .get(
+        `http://127.0.0.1:3000/photos-short-list?_page=${stringPage}&_limit=10`
+      )
+      .then((res) => {
+        if (res.data.length === 0) {
+          return;
+        }
+        setResults((otherResults) => [...otherResults, ...res.data]);
+      })
+      .catch((err) => console.log(err));
+  }
 
   useEffect(() => {
     const stringPage = page.toString();
     axios
       .get(
-        `http://127.0.0.1:3000/photos-short-list?_page=${stringPage}&_limit=5`
+        `http://127.0.0.1:3000/photos-short-list?_page=${stringPage}&_limit=10`
       )
       .then((res) => setResults(res.data))
       .catch((err) => console.log(err));
   }, []);
 
-  console.log(results);
+  useEffect(() => {
+    results && getColorSquares();
+  }, [results]);
+
   return (
     <>
       {" "}
-      <div className="grid">
-        <img src="https://via.placeholder.com/600/92c952" />
-        <img src="https://via.placeholder.com/600/771796" />
-        <img src="https://via.placeholder.com/600/24f355" />
-        <img src="https://via.placeholder.com/600/d32776" />
-        <img src="https://via.placeholder.com/600/f66b97" />
-        <img src="https://via.placeholder.com/600/92c952" />
-        <div className="skeleton">Loading...</div>
-        <div className="skeleton">Loading...</div>
-        <div className="skeleton">Loading...</div>
-        <div className="skeleton">Loading...</div>
-        <div className="skeleton">Loading...</div>
-        <div className="skeleton">Loading...</div>
-
-        {results?.map((item) => {
-          return <img key={item.id} src={item.url} />;
+      <div className="grid" ref={gridRef}>
+        {results?.map((item, index) => {
+          return (
+            <Fragment key={index}>
+              <img
+                id="colorSquare"
+                className={index === results.length - 1 ? "last" : ""}
+                src={item.url}
+              />
+              {index === results.length - 1 ? (
+                <div>This is the end of the list</div>
+              ) : (
+                <></>
+              )}
+            </Fragment>
+          );
         })}
       </div>
     </>

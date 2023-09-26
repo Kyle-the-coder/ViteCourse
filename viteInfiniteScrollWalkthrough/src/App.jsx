d21ba1a15d47fd280fad08d11f9086cc4a7ec416
Kyle Mitchell <kylethacoder@gmail.com>
2023-09-26 09:42:10 -0700

    updated

--- viteInfiniteScrollWalkthrough/src/App.jsx
+++ viteInfiniteScrollWalkthrough/src/App.jsx
@@ -1,3 +1,20 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 export default function App() {
-  return <h1>hi</h1>;
+  const [photos, setPhotos] = useState("");
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/photos?_page=1&_limit=10")
+      .then((res) => setPhotos(res.data))
+      .catch((err) => console.log(err));
+  }, []);
+  console.log(photos);
+  return (
+    <div className="grid">
+      {photos?.map((photo) => {
+        return <img src={photo.url} key={photo.id} />;
+      })}
+    </div>
+  );
 }

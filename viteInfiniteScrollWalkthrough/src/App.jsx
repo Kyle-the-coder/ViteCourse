158555c3303eaa606cf6abaf1c6156e700511f94
Kyle Mitchell <kylethacoder@gmail.com>
2023-09-26 10:11:55 -0700

    updated

--- viteInfiniteScrollWalkthrough/src/App.jsx
+++ viteInfiniteScrollWalkthrough/src/App.jsx
@@ -1,20 +1,68 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
+import { parseLinkHeader } from "./parseLinkHeader";
 
+const LIMIT = 10;
 export default function App() {
+  const [isLoading, setIsLoading] = useState(false);
   const [photos, setPhotos] = useState([]);
+  const nextPhotoUrlRef = useRef();
+
+  async function fetchPhotos(url, { overwrite = false } = {}) {
+    setIsLoading(true);
+    try {
+      await new Promise((res) => setTimeout(res, 2000));
+      const res = await fetch(url);
+      nextPhotoUrlRef.current = parseLinkHeader(res.headers.get("Link")).next;
+      const photos = await res.json();
+      if (overwrite) {
+        setPhotos(photos);
+      } else {
+        setPhotos((prevPhotos) => [...prevPhotos, ...photos]);
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  const imageRef = useCallback((image) => {
+    if (image === null || nextPhotoUrlRef.current === null) return;
+    const observer = new IntersectionObserver((entries) => {
+      if (entries[0].isIntersecting) {
+        //TODO: LOAD NEXT ELEMENTS
+        fetchPhotos(nextPhotoUrlRef.current);
+        observer.unobserve(image);
+      }
+    });
+    observer.observe(image);
+  }, []);
   useEffect(() => {
-    axios
-      .get("http://localhost:3000/photos?_page=1&_limit=10")
-      .then((res) => setPhotos(res.data))
-      .catch((err) => console.log(err));
+    fetchPhotos(`http://localhost:3000/photos?_page=1&_limit=${LIMIT}`, {
+      overwrite: true,
+    });
   }, []);
 
   return (
     <div className="grid">
-      {photos.map((photo) => {
-        return <img src={photo.url} key={photo.id} />;
+      {photos.map((photo, index) => {
+        return (
+          <img
+            src={photo.url}
+            ref={index === photos.length - 1 ? imageRef : undefined}
+            key={photo.id}
+          />
+        );
       })}
+      {isLoading &&
+        Array.from({ length: LIMIT }, (_, index) => index).map((n) => {
+          return (
+            <div key={n} className="skeleton">
+              Loading
+            </div>
+          );
+        })}
     </div>
   );
 }

e3bd0471c6038d13ab780769f4290a32b2c62761
Kyle Mitchell <kylethacoder@gmail.com>
2023-09-26 09:43:20 -0700

    updated

--- viteInfiniteScrollWalkthrough/src/App.jsx
+++ viteInfiniteScrollWalkthrough/src/App.jsx
@@ -2,17 +2,17 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 export default function App() {
-  const [photos, setPhotos] = useState("");
+  const [photos, setPhotos] = useState([]);
   useEffect(() => {
     axios
       .get("http://localhost:3000/photos?_page=1&_limit=10")
       .then((res) => setPhotos(res.data))
       .catch((err) => console.log(err));
   }, []);
-  console.log(photos);
+
   return (
     <div className="grid">
-      {photos?.map((photo) => {
+      {photos.map((photo) => {
         return <img src={photo.url} key={photo.id} />;
       })}
     </div>

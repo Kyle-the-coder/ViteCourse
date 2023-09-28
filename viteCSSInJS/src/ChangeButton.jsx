d1132f14f372fe571b22c96554d467ac6bb4af2a
Kyle Mitchell <kylethacoder@gmail.com>
2023-09-28 10:53:40 -0700

    updated

---
+++ viteCSSInJS/src/ChangeButton.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const ChangeButton = styled.button`
+  background-color: aliceblue;
+  border-radius: 1rem;
+  padding: 6px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  border: 2px solid black;
+`;

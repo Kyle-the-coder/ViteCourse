cb50a80f8b97b62ca8a23fbd43e2f9d3fbae681c
Kyle Mitchell <kylethacoder@gmail.com>
2023-09-28 10:13:56 -0700

    updated

--- viteCSSModules/src/App.jsx
+++ viteCSSModules/src/App.jsx
@@ -1,3 +1,11 @@
+import { Child } from "./Child";
+import "./parent.css";
+
 export default function App() {
-  return <div>Hello Child</div>;
+  return (
+    <div>
+      <h1 className="header">App</h1>
+      <Child />
+    </div>
+  );
 }

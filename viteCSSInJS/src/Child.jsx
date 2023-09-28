e0647f9a982c5ca8f343892cc304bee153350bd4
Kyle Mitchell <kylethacoder@gmail.com>
2023-09-28 10:53:44 -0700

    updated

--- viteCSSInJS/src/Child.jsx
+++ viteCSSInJS/src/Child.jsx
@@ -1,7 +1,32 @@
+import { useEffect, useState } from "react";
 import styled from "styled-components";
+import { ChangeButton } from "./ChangeButton";
+
 export function Child() {
+  const [color, setColor] = useState("red");
+  const [isColor, setIsColor] = useState(false);
+  useEffect(() => {
+    if (color === "red") {
+      setColor("pink");
+    }
+  }, []);
   const RedHOne = styled.h1`
-    color: red;
+    color: ${(props) => props.color};
   `;
-  return <RedHOne>Child</RedHOne>;
+
+  function noColor() {
+    setIsColor(!isColor);
+    if (!isColor) {
+      setColor("black");
+    } else if (isColor) {
+      setColor("pink");
+    }
+  }
+
+  return (
+    <>
+      <RedHOne color={color}>Child</RedHOne>
+      <ChangeButton onClick={() => noColor()}>Change Color</ChangeButton>
+    </>
+  );
 }

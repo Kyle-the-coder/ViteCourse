3360001c895b9a8a0909ff5aa988818c42318e88
Kyle Mitchell <kylethacoder@gmail.com>
2023-09-29 11:36:16 -0700

    updated

--- viteTypeScript/src/index.ts
+++ viteTypeScript/src/index.ts
@@ -1,1 +1,36 @@
 let age: number = 20;
+let course: string = "TypeScript";
+let isPublished: boolean = true;
+let numbers: number[] = [1, 2, 3];
+//Tuple
+let user: [number, string] = [1, "Jean"];
+//Enum
+enum Size {
+  Small = 1,
+  Medium,
+  Large,
+}
+let mySize: Size = Size.Large;
+
+function render(document: any) {
+  console.log(document);
+}
+
+function calcTax(income: number) {
+  return 0;
+}
+
+//Object
+
+type Employee = {
+  readonly id: number;
+  name: string;
+  retire: (date: Date) => void;
+};
+let employee: Employee = {
+  id: 1,
+  name: "Kyle",
+  retire: (date: Date) => {
+    console.log(date);
+  },
+};

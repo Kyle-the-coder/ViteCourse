7586c033ec3a29f04e88f4ae5c1702eab5a7ae46
Kyle Mitchell <kylethacoder@gmail.com>
2023-09-29 10:06:40 -0700

    updated

---
+++ viteReactBootstrap/src/App.jsx
@@ -0,0 +1,9 @@
+import Button from "react-bootstrap/Button";
+export default function App() {
+  return (
+    <div>
+      <h1>App</h1>
+      <Button variant="danger">Button</Button>
+    </div>
+  );
+}

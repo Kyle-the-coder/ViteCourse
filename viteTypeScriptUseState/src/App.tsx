bdeea094e7fc964b7c4dd8503444d50bc7f5993c
Kyle Mitchell <kylethacoder@gmail.com>
2023-09-30 11:12:35 -0700

    updated

---
+++ viteTypeScriptUseState/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { useState } from "react";
+
+export default function App() {
+  const [value, setValue] = useState<string>();
+  // const [value, setValue] = useState<string[]>([])
+  return (
+    <input
+      type="text"
+      value={value}
+      onChange={(e) => setValue(e.target.value)}
+    />
+  );
+}

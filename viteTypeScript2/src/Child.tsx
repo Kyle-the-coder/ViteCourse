fce76110b7e4def0d452e530f103f86d864d31c8
Kyle Mitchell <kylethacoder@gmail.com>
2023-09-30 11:08:32 -0700

    updated

---
+++ viteTypeScript2/src/Child.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import type { ReactNode } from "react";
+
+type ChildProps = {
+  children: ReactNode;
+};
+export function Child({ children }: ChildProps) {
+  return <h1> {children}</h1>;
+}

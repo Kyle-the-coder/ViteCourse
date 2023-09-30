56515d3ed2d880261a58cdec8f4fefbbc5d07245
Kyle Mitchell <kylethacoder@gmail.com>
2023-09-30 11:08:25 -0700

    updated

---
+++ viteTypeScript2/src/Button.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import type { ComponentProps } from "react";
+
+type ButtonProps = {
+  outline?: boolean;
+} & ComponentProps<"button">;
+
+export function Button({ outline, ...props }: ButtonProps) {
+  return (
+    <button
+      style={{ border: outline ? "1px solid red" : undefined }}
+      {...props}
+    ></button>
+  );
+}

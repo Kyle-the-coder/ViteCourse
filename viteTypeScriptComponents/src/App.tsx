6c06f969bd305303d80562681233fdf361d52b3e
Kyle Mitchell <kylethacoder@gmail.com>
2023-10-01 12:43:18 -0700

    updated

---
+++ viteTypeScriptComponents/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { List } from "./List";
+export default function App() {
+  return (
+    <List<{ id: number; name: string; age?: number }>
+      items={[
+        { id: 1, name: "Kyle" },
+        { id: 2, name: "Sally" },
+      ]}
+      getKey={(item) => item.id}
+      renderItem={(item) => <div>{item.name}</div>}
+    />
+  );
+}

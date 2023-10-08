3ebcec27ee1ce81e97710baa1835daa0f7a4e121
Kyle Mitchell <kylethacoder@gmail.com>
2023-10-08 11:22:54 -0700

    updated

--- viteGCCWalkthroughTS/src/modals/EventFormModal.tsx
+++ viteGCCWalkthroughTS/src/modals/EventFormModal.tsx
@@ -1,13 +1,14 @@
 import React from "react";
-import { Modal } from "../components/Modal";
+import { Modal, ModalProps } from "../components/Modal";
 import { UnionOmit } from "../utils/types";
 
 type EventFormModalProps = {
   onSubmit: (event: UnionOmit<Event, "id">) => void;
-  onDelete: () => void;
-  event: Event;
-  date: Date;
-};
+} & (
+  | { onDelete: () => void; event: Event; date?: Date }
+  | { onDelete?: never; event?: never; date: Date }
+) &
+  Omit<ModalProps, "children">;
 
 export function EventFormModal({
   onSubmit,
@@ -15,6 +16,78 @@ export function EventFormModal({
   event,
   date,
   ...modalProps
-}) {
-  return <Modal {...modalProps}></Modal>;
+}: EventFormModalProps) {
+  return (
+    <Modal {...modalProps}>
+      <div className="modal-title">
+        <div>Add Event</div>
+        <small>6/8/23</small>
+        <button className="close-btn">&times;</button>
+      </div>
+      <form>
+        <div className="form-group">
+          <label htmlFor="name">Name</label>
+          <input type="text" name="name" id="name" />
+        </div>
+        <div className="form-group checkbox">
+          <input type="checkbox" name="all-day" id="all-day" />
+          <label htmlFor="all-day">All Day?</label>
+        </div>
+        <div className="row">
+          <div className="form-group">
+            <label htmlFor="start-time">Start Time</label>
+            <input type="time" name="start-time" id="start-time" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="end-time">End Time</label>
+            <input type="time" name="end-time" id="end-time" />
+          </div>
+        </div>
+        <div className="form-group">
+          <label>Color</label>
+          <div className="row left">
+            <input
+              type="radio"
+              name="color"
+              value="blue"
+              id="blue"
+              checked
+              className="color-radio"
+            />
+            <label htmlFor="blue">
+              <span className="sr-only">Blue</span>
+            </label>
+            <input
+              type="radio"
+              name="color"
+              value="red"
+              id="red"
+              className="color-radio"
+            />
+            <label htmlFor="red">
+              <span className="sr-only">Red</span>
+            </label>
+            <input
+              type="radio"
+              name="color"
+              value="green"
+              id="green"
+              className="color-radio"
+            />
+            <label htmlFor="green">
+              <span className="sr-only">Green</span>
+            </label>
+          </div>
+        </div>
+        <div className="row">
+          <button className="btn btn-success" type="submit">
+            Add
+          </button>
+          <button className="btn btn-delete" type="button">
+            Delete
+          </button>
+        </div>
+      </form>
+    </Modal>
+  );
 }

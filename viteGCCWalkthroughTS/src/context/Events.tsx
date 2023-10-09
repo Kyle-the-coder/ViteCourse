48805f7b800d2560b12b2709f785297d9f96bb78
Kyle Mitchell <kylethacoder@gmail.com>
2023-10-09 11:54:59 -0700

    updated

--- viteGCCWalkthroughTS/src/context/Events.tsx
+++ viteGCCWalkthroughTS/src/context/Events.tsx
@@ -15,6 +15,8 @@ export type Event = {
 type EventsContext = {
   events: Event[];
   addEvent: (event: UnionOmit<Event, "id">) => void;
+  updateEvent: (id: string, eventDetails: UnionOmit<Event, "id">) => void;
+  deleteEvent: (id: string) => void;
 };
 
 export const Context = createContext<EventsContext | null>(null);
@@ -26,10 +28,22 @@ type EventsProviderProps = {
 export function EventsProvider({ children }: EventsProviderProps) {
   const [events, setEvents] = useState<Event[]>([]);
 
-  function addEvent(event: UnionOmit<Event, "id">) {
-    setEvents((e) => [...e, { ...event, id: crypto.randomUUID() }]);
+  function addEvent(eventDetails: UnionOmit<Event, "id">) {
+    setEvents((e) => [...e, { ...eventDetails, id: crypto.randomUUID() }]);
+  }
+  function updateEvent(id: string, eventDetails: UnionOmit<Event, "id">) {
+    setEvents((e) => {
+      return e.map((event) => {
+        event.id === id ? { id, ...eventDetails } : event;
+      });
+    });
+  }
+  function deleteEvent(id: string) {
+    setEvents((e) => e.filter((event) => event.id !== id));
   }
   return (
-    <Context.Provider value={{ events, addEvent }}>{children}</Context.Provider>
+    <Context.Provider value={{ events, addEvent, updateEvent, deleteEvent }}>
+      {children}
+    </Context.Provider>
   );
 }

3b8bc6a4de5a9742b79f43697426906c4663145b
Kyle Mitchell <kylethacoder@gmail.com>
2023-10-09 11:17:59 -0700

    updated

--- viteGCCWalkthroughTS/src/components/Calendar.tsx
+++ viteGCCWalkthroughTS/src/components/Calendar.tsx
@@ -4,6 +4,7 @@ import {
   endOfMonth,
   endOfWeek,
   isBefore,
+  isSameDay,
   isSameMonth,
   isToday,
   startOfMonth,
@@ -26,6 +27,8 @@ export function Calendar() {
       end: lastWeekEnd,
     });
   }, [selectedMonth]);
+  const { events } = useEvents();
+
   return (
     <div className="calendar">
       <div className="header">
@@ -61,6 +64,7 @@ export function Calendar() {
             day={day}
             showWeekName={index < 7}
             selectedMonth={selectedMonth}
+            events={events.filter((event) => isSameDay(day, event.date))}
           />
         ))}
       </div>
@@ -72,11 +76,33 @@ type CalendarDayProps = {
   day: Date;
   showWeekName: boolean;
   selectedMonth: Date;
+  events: Event[];
 };
 
-function CalendarDay({ day, showWeekName, selectedMonth }: CalendarDayProps) {
+function CalendarDay({
+  day,
+  showWeekName,
+  selectedMonth,
+  events,
+}: CalendarDayProps) {
   const [isNewEventModalOpen, setIsNewEventModalOpen] = useState(false);
   const { addEvent } = useEvents();
+
+  const sortedEvents = useMemo(() => {
+    const timeToNumber = (time: string) => parseFloat(time.replace(":", "."));
+    return [...events].sort((a, b) => {
+      if (a.allDay && b.allDay) {
+        return 0;
+      } else if (a.allDay) {
+        return -1;
+      } else if (b.allDay) {
+        return 1;
+      } else {
+        return timeToNumber(a.startTime) - timeToNumber(b.startTime);
+      }
+    });
+  }, [events]);
+
   return (
     <div
       className={cc(
@@ -101,21 +127,24 @@ function CalendarDay({ day, showWeekName, selectedMonth }: CalendarDayProps) {
           +
         </button>
       </div>
-      {/* <div className="events">
-            <button className="all-day-event blue event">
-              <div className="event-name">Short</div>
-            </button>
-            <button className="all-day-event green event">
-              <div className="event-name">
-                Long Event Name That Just Keeps Going
-              </div>
-            </button>
-            <button className="event">
-              <div className="color-dot blue"></div>
-              <div className="event-time">7am</div>
-              <div className="event-name">Event Name</div>
-            </button>
-          </div> */}
+      {sortedEvents.length > 0 && (
+        <div className="events">
+          <button className="all-day-event blue event">
+            <div className="event-name">Short</div>
+          </button>
+          <button className="all-day-event green event">
+            <div className="event-name">
+              Long Event Name That Just Keeps Going
+            </div>
+          </button>
+          <button className="event">
+            <div className="color-dot blue"></div>
+            <div className="event-time">7am</div>
+            <div className="event-name">Event Name</div>
+          </button>
+        </div>
+      )}
+
       <EventFormModal
         date={day}
         isOpen={isNewEventModalOpen}
@@ -123,5 +152,13 @@ function CalendarDay({ day, showWeekName, selectedMonth }: CalendarDayProps) {
         onSubmit={addEvent}
       />
     </div>
+  );
+}
+
+function CalendarEvent({ event }: { event: Event }) {
+  return (
+    <button className="all-day-event blue event">
+      <div className="event-name">Short</div>
+    </button>
   );
 }

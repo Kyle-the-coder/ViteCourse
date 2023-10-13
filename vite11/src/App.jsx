6faaf55e30422bae274e78812e9063c952cc9f5b
Kyle Mitchell <kylethacoder@gmail.com>
2023-10-13 12:01:16 -0700

    updated

--- vite11/src/App.jsx
+++ vite11/src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from "react";
-import Counter from "./Counter";
+import { Counter } from "./Counter";
 
 export default function App() {
   const [name, setName] = useState("");
+  const [intialCount, setInitialCount] = useState(0);
+
   return (
     <>
       <input value={name} onChange={(e) => setName(e.target.value)} />
+      <button onClick={() => setInitialCount((c) => c + 1)}>+</button>
       <br />
-      <Counter intialCount={0} />
+      <Counter intialCount={intialCount} otherProp="hi" />
     </>
   );
 }

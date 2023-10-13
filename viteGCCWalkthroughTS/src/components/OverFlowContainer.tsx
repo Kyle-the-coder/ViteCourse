8485b2b4e5c06110ed6b68b10d45d1c4ebadd1bd
Kyle Mitchell <kylethacoder@gmail.com>
2023-10-13 10:25:32 -0700

    updated

--- viteGCCWalkthroughTS/src/components/OverFlowContainer.tsx
+++ viteGCCWalkthroughTS/src/components/OverFlowContainer.tsx
@@ -1,4 +1,10 @@
-import React, { Key, ReactNode } from "react";
+import React, {
+  Key,
+  ReactNode,
+  useLayoutEffect,
+  useRef,
+  useState,
+} from "react";
 
 type OverflowContainerProps<T> = {
   items: T[];
@@ -15,14 +21,34 @@ export function OverFlowContainer<T>({
   renderOverflow,
   className,
 }: OverflowContainerProps<T>) {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const [overflowAmount, setOverflowAmout] = useState(0);
+
+  useLayoutEffect(() => {
+    if (containerRef.current === null) return;
+    const observer = new ResizeObserver((entries) => {
+      const containerElement = entries[0]?.target;
+      if (containerElement === null) return;
+      console.log(containerElement);
+    });
+
+    observer.observe(containerRef.current);
+    return () => {
+      observer.disconnect();
+    };
+  }, [items]);
   return (
     <>
-      <div className={className}>
+      <div className={className} ref={containerRef}>
         {items.map((item) => {
-          return <div key={getKey(item)}>{renderItem(item)}</div>;
+          return (
+            <div data-item key={getKey(item)}>
+              {renderItem(item)}
+            </div>
+          );
         })}
       </div>
-      <div>{renderOverflow(overflowAmount)}</div>
+      <div data-overflow>{renderOverflow(overflowAmount)}</div>
     </>
   );
 }
